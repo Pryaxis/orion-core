a636c05e17788f521e38a294938f14228ebb6e64
White <monsieur_white@hotmail.com>
2015-05-05 15:35:23 +0930

    Log4Net binary

---
+++ Orion/Logging/Log.cs
@@ -0,0 +1,17 @@
+﻿using log4net.Appender;
+using log4net.Config;
+
+namespace Orion.Logging
+{
+	public sealed class LogAppender : RollingFileAppender
+	{
+	}
+
+	public sealed class Log
+	{
+		public Log()
+		{
+			XmlConfigurator.Configure();
+		}
+	}
+}
