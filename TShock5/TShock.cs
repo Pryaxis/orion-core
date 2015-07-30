903ef0a87be1c178ea5885c11d5829d5bd3ad175
White <monsieur_white@hotmail.com>
2015-07-30 10:19:19 +0930

    Initial commit of the TShock5 project

---
+++ TShock5/TShock.cs
@@ -0,0 +1,55 @@
+﻿using Orion;
+using System;
+using System.Reflection;
+
+namespace TShock5
+{
+	[OrionVersion(1, 0)]
+	public class TShock : OrionPlugin
+	{
+		public override string Author
+		{
+			get
+			{
+				return "Nyx Studios";
+			}
+		}
+
+		public override string Name
+		{
+			get
+			{
+				return "TShock 5";
+			}
+		}
+
+		public override int Order
+		{
+			get
+			{
+				return 1;
+			}
+		}
+
+		public override Version Version
+		{
+			get
+			{
+				return Assembly.GetExecutingAssembly().GetName().Version;
+			}
+		}
+		protected TShock(Orion.Orion instance) :
+			base(instance)
+		{
+		}
+
+		public override void Initialize()
+		{
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			Dispose();
+		}
+	}
+}
