54dbef4a09adebe306514ea466df36128c524dc4
White <monsieur_white@hotmail.com>
2015-07-30 10:19:03 +0930

    Added groups and a group handler. This still needs some revision.

---
+++ Orion/Grouping/Group.cs
@@ -0,0 +1,62 @@
+﻿using Orion.Extensions;
+using Orion.SQL;
+using Orion.Permissions;
+
+namespace Orion.Grouping
+{
+	public class Group
+	{
+		public int ID { get; private set; }
+		public string Name { get; private set; }
+		public PermissionCollection Permissions { get; private set; }
+		public Color ChatColor { get; private set; }
+		public string Prefix { get; private set; }
+		public string Suffix { get; private set; }
+
+		/// <summary>Default constructor for a Group object; holds no data.</summary>
+		/// <returns>A Group object.</returns>
+		public Group()
+		{
+			ID = -1;
+			Name = "";
+			Permissions = new PermissionCollection();
+			ChatColor = Color.White;
+			Prefix = "";
+			Suffix = "";
+		}
+
+		public Group(string name, PermissionCollection permissions, Color chatColor, string prefix, string suffix)
+		{
+			Name = name;
+			Permissions = permissions;
+			ChatColor = chatColor;
+			Prefix = prefix;
+			Suffix = suffix;
+		}
+
+		/// <summary>
+		/// Checks if this group's permissions contains the given permission
+		/// </summary>
+		/// <param name="permission"></param>
+		/// <returns></returns>
+		public bool HasPermission(string permission)
+		{
+			if (Permissions.Negated(permission))
+			{
+				return false;
+			}
+
+			return Permissions.Contains(permission);
+		}
+		
+		public void LoadFromQuery(QueryResult result)
+		{
+			ID = result.Get<int>("ID");
+			Name = result.Get<string>("Name");
+			Permissions = new PermissionCollection(result.Get<string>("Permissions"));
+			ChatColor = result.Get<string>("ChatColor").ToColor();
+			Prefix = result.Get<string>("Prefix");
+			Suffix = result.Get<string>("Suffix");
+		}
+	}
+}
---
+++ Orion/Grouping/GroupHandler.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using Orion.Exceptions;
+using Orion.Extensions;
+using Orion.SQL;
+using Orion.UserAccounts;
+
+namespace Orion.Grouping
+{
+	public class GroupHandler
+	{
+		private Orion _core;
+		public readonly IDbConnection database;
+
+		public GroupHandler(Orion core)
+		{
+			_core = core;
+			database = core.Database;
+
+			SqlTable table = new SqlTable("Groups",
+				new SqlColumn("ID", MySqlDbType.Int32) { Primary = true, AutoIncrement = true },
+				new SqlColumn("Name", MySqlDbType.VarChar, 32) { Unique = true },
+				new SqlColumn("Permissions", MySqlDbType.VarChar, 128),
+				new SqlColumn("ChatColor", MySqlDbType.VarChar, 128),
+				new SqlColumn("Prefix", MySqlDbType.Text),
+				new SqlColumn("Suffix", MySqlDbType.Text)
+				);
+
+			SqlTableCreator creator = new SqlTableCreator(database,
+				database.GetSqlType() == SqlType.Sqlite
+					? (IQueryBuilder)new SqliteQueryCreator()
+					: new MysqlQueryCreator());
+			creator.EnsureTableStructure(table);
+		}
+
+		public void SetAccountGroup(UserAccount account, string groupName)
+		{
+			Group g = GetGroupFromName(groupName);
+			if (g != null)
+			{
+				account.Group = g;
+			}
+			else
+			{
+				throw new GroupNotFoundException(groupName);
+			}
+		}
+		
+		public Group GetGroupFromName(string name)
+		{
+			using (QueryResult result = database.QueryReader("SELECT * FROM Groups WHERE Name=@0", name))
+			{
+				if (result.Read())
+				{
+					Group g = new Group();
+					g.LoadFromQuery(result);
+					return g;
+				}
+			}
+
+			return null;
+		}
+
+		public Group GetGroupFromID(int ID)
+		{
+			using (QueryResult result = database.QueryReader("SELECT * FROM Groups WHERE ID=@0", ID))
+			{
+				if (result.Read())
+				{
+					Group g = new Group();
+					g.LoadFromQuery(result);
+					return g;
+				}
+			}
+
+			return null;
+		}
+	}
+}
