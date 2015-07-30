fa1514c4c7ce8d37cb55a5a08ff8ea14c3238bbf
White <monsieur_white@hotmail.com>
2015-07-30 10:14:08 +0930

    Added an extension method to parse a colour from a string

--- Orion/Extensions/StringEx.cs
+++ Orion/Extensions/StringEx.cs
@@ -1,7 +1,34 @@
-﻿namespace Orion.Extensions
+﻿using System;
+
+namespace Orion.Extensions
 {
 	public static class StringEx
 	{
-		//empty
+		public static Color ToColor(this string str)
+		{
+			byte r, g, b;
+			string[] pieces = str.Split(',');
+			if (pieces.Length < 3 || pieces.Length > 3)
+			{
+				throw new ArgumentException("Provided string must be in the format rrr,ggg,bbb", "str");
+			}
+
+			if (!Byte.TryParse(pieces[0], out r))
+			{
+				throw new ArgumentException("Provided red value is not a byte value.", "str");
+			}
+
+			if (!Byte.TryParse(pieces[1], out g))
+			{
+				throw new ArgumentException("Provided green value is not a byte value.", "str");
+			}
+
+			if (!Byte.TryParse(pieces[2], out b))
+			{
+				throw new ArgumentException("Provided blue value is not a byte value.", "str");
+			}
+
+			return new Color(r, g, b);
+		}
 	}
 }
