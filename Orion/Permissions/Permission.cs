f952ca4d2375e5f2cac5febec5a18bc3870e6828
White <monsieur_white@hotmail.com>
2015-07-30 10:16:31 +0930

    Added more to Permission and PermissionCollection classes

--- Orion/Permissions/Permission.cs
+++ Orion/Permissions/Permission.cs
@@ -14,5 +14,35 @@
 			}
 			Name = name;
 		}
+
+		public bool Equals(Permission permission)
+		{
+			return permission.Negate = Negate && permission.Name == Name;
+		}
+
+		public bool Equals(string permission)
+		{
+			if (Negate && !permission.StartsWith("!"))
+			{
+				return false;
+			}
+			
+			if (Negate && permission.StartsWith("!"))
+			{
+				return permission.Remove(0, 1) == Name;
+			}
+
+			if (!Negate && permission.StartsWith("!"))
+			{
+				return false;
+			}
+
+			return permission == Name;
+		}
+
+		public override string ToString()
+		{
+			return (Negate ? "!" : "") + Name;
+		}
 	}
 }
--- Orion/Permissions/PermissionCollection.cs
+++ Orion/Permissions/PermissionCollection.cs
@@ -1,11 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Orion.Permissions
 {
-	public class PermissionCollection
+	/// <summary>
+	/// A collection of <see cref="Permission"></see>s
+	/// </summary>
+	public class PermissionCollection : ICollection
 	{
 		private List<Permission> _permissions;
 
+		public int Count
+		{
+			get
+			{
+				return ((ICollection)_permissions).Count;
+			}
+		}
+
+		public object SyncRoot
+		{
+			get
+			{
+				return ((ICollection)_permissions).SyncRoot;
+			}
+		}
+
+		public bool IsSynchronized
+		{
+			get
+			{
+				return ((ICollection)_permissions).IsSynchronized;
+			}
+		}
+
+		public Permission this[int index]
+		{
+			get
+			{
+				return _permissions[index];
+			}
+			set
+			{
+				_permissions[index] = value;
+			}
+		}
+
+		/// <summary>
+		/// Creates a new PermissionCollection using a comma-separated string of permissions
+		/// </summary>
+		/// <param name="permissions"></param>
 		public PermissionCollection(string permissions)
 		{
 			_permissions = new List<Permission>();
@@ -16,11 +62,18 @@ namespace Orion.Permissions
 			}
 		}
 
+		/// <summary>
+		/// Creates an empty PermissionCollection
+		/// </summary>
 		public PermissionCollection()
 		{
 			_permissions = new List<Permission>();
 		}
 
+		/// <summary>
+		/// Creates a new PermissionCollection using a list of strings
+		/// </summary>
+		/// <param name="permissions"></param>
 		public PermissionCollection(List<string> permissions)
 		{
 			_permissions = new List<Permission>();
@@ -31,15 +84,76 @@ namespace Orion.Permissions
 			}
 		}
 
+		/// <summary>
+		/// Adds a new permission to the collection
+		/// </summary>
+		/// <param name="permission"></param>
 		public void Add(string permission)
 		{
 			_permissions.Add(new Permission(permission));
 		}
 
+		/// <summary>
+		/// Removes a permission from the collection
+		/// </summary>
+		/// <param name="permission"></param>
 		public void Remove(string permission)
 		{
-			Permission p = new Permission(permission);
-			_permissions.RemoveAll(perm => perm.Name == p.Name);
+			_permissions.RemoveAll(p => p.Equals(permission));
+		}
+
+		/// <summary>
+		/// Removes a permission from the collection
+		/// </summary>
+		/// <param name="permission"></param>
+		public void Remove(Permission permission)
+		{
+			_permissions.RemoveAll(p => p.Equals(permission));
+		}
+
+		/// <summary>
+		/// Determines whether or not the collection contains a permission
+		/// </summary>
+		/// <param name="permission"></param>
+		/// <returns></returns>
+		public bool Contains(string permission)
+		{
+			return _permissions.Any(p => p.Equals(permission));
+		}
+
+		/// <summary>
+		/// Determines whether or not the collection contains a permission
+		/// </summary>
+		/// <param name="permission"></param>
+		/// <returns></returns>
+		public bool Contains(Permission permission)
+		{
+			return _permissions.Any(p => p.Equals(permission));
+		}
+
+		public bool Negated(string permission)
+		{
+			return _permissions.Any(p => p.Equals(permission) && p.Negate);
+		}
+
+		public bool Negated(Permission permission)
+		{
+			return _permissions.Any(p => p.Equals(permission) && p.Negate);
+		}
+
+		public void CopyTo(Array array, int index)
+		{
+			((ICollection)_permissions).CopyTo(array, index);
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return ((ICollection)_permissions).GetEnumerator();
+		}
+
+		public override string ToString()
+		{
+			return String.Join(",", _permissions.Select(p => p.ToString()));
 		}
 	}
 }
