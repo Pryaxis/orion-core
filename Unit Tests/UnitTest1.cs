0e6a463138d6d97f19d1e156c5765c2b388186e5
White <monsieur_white@hotmail.com>
2015-08-06 18:44:32 +0930

    UnitTest now contains a test for PointCollections

--- Unit Tests/UnitTest1.cs
+++ Unit Tests/UnitTest1.cs
@@ -1,64 +1,41 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Orion.Net;
-using Orion;
+using Orion.Regions;
 
 namespace Unit_Tests
 {
 	[TestClass]
-	public class UnitTest1 : OrionPlugin
+	public class WindingPointTest
 	{
-		public override string Author
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-		}
-
-		public override string Name
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-		}
-
-		public override int Order
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-		}
-
-		public override Version Version
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-		}
-
-		public override void Initialize()
-		{
-			throw new NotImplementedException();
-		}
-
-		protected UnitTest1(Orion.Orion instance)
-			: base(instance)
-		{
-		}
+		PointCollection pc;
 
 		[TestMethod]
 		public void TestMethod1()
 		{
-			
-		}
+			pc = new PointCollection();
+			pc.AddPoint(0, 0);
+			pc.AddPoint(0, 10);
+			pc.AddPoint(10, 10);
+			pc.AddPoint(10, 20);
+			pc.AddPoint(15, 15);
+			pc.AddPoint(30, 30);
+			pc.AddPoint(30, 20);
+			pc.AddPoint(20, 10);
+			pc.AddPoint(15, 5);
+			pc.AddPoint(20, 0);
 
-		protected override void Dispose(bool disposing)
-		{
-			throw new NotImplementedException();
+			Console.WriteLine(pc.IsInArea(5, 5));
+			Console.WriteLine(pc.IsInArea(10, 10));
+			Console.WriteLine(pc.IsInArea(25, 25));
+			Console.WriteLine(pc.IsInArea(15, 16));
+			Console.WriteLine(pc.IsInArea(20, 0));
+
+			//Expected   |    Received
+			//true            true
+			//true            true
+			//true            true
+			//false           false
+			//true            true
 		}
 	}
 }
