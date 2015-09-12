99bd45061ce1f1a9c2ae672c228fd4b272d9cf68
White <monsieur_white@hotmail.com>
2015-09-12 16:03:19 +0930

    Updated the attribute constructor of NetItem and added some comments

--- Orion/Net/NetItem.cs
+++ Orion/Net/NetItem.cs
@@ -7,14 +7,40 @@
 		public short Stack { get; set; }
 		public bool Favorite { get; set; }
 
+		/// <summary>
+		/// Constructs a new <see cref="NetItem"></see> from item attributes
+		/// </summary>
+		/// <param name="id">A valid <see cref="Terraria.ID.ItemID"></see></param>
+		/// <param name="prefix">Item prefix</param>
+		/// <param name="stack">Item stack</param>
+		/// <param name="favorite">Item favorite status</param>
 		public NetItem(short id, byte prefix, short stack, bool favorite)
 		{
+			if (id < -24 || id >= Terraria.ID.ItemID.Count)
+			{
+				id = 0;
+			}
 			ID = id;
+
+			if (prefix < 0 || prefix > Terraria.Item.maxPrefixes)
+			{
+				prefix = 0;
+			}
 			Prefix = prefix;
+
+			if (stack < 0 || stack > 999)
+			{
+				stack = 0;
+			}
 			Stack = stack;
+
 			Favorite = favorite;
 		}
 
+		/// <summary>
+		/// Constructs a new <see cref="NetItem"></see> from a <see cref="Terraria.Item"></see>
+		/// </summary>
+		/// <param name="item"><see cref="Terraria.Item"></see> object</param>
 		public NetItem(Terraria.Item item)
 		{
 			ID = (short)item.netID;
@@ -23,6 +49,10 @@
 			Favorite = item.favorited;
 		}
 
+		/// <summary>
+		/// Constructs a new <see cref="NetItem"></see> from a <see cref="Terraria.Item"></see>
+		/// </summary>
+		/// <param name="item"></param>
 		public static implicit operator NetItem(Terraria.Item item)
 		{
 			if (item == null)
