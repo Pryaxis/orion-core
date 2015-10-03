e6b796179ee507f06d66bb204e2d4031dfd26ec4
Tyler Watson <tyler@tw.id.au>
2015-10-03 23:45:51 +1000

    OrionPlugin implementation, it works!

---
+++ Orion/OrionPlugin.cs
@@ -0,0 +1,27 @@
+﻿using OTA.Logging;
+using OTA.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion
+{
+    public class OrionPlugin : BasePlugin
+    {
+        public OrionPlugin() 
+            : base()
+        {
+            Author = "Nyx Studios";
+            Description = "Plugin that exposes a comprehensive API for TShock v5+";
+            Version = this.GetType().Assembly.GetName().Version.ToString();
+        }
+
+        protected override void Initialized(object state)
+        {
+            Version orionVersion = this.GetType().Assembly.GetName().Version;
+            ProgramLog.Log($"Orion version {orionVersion.ToString()} initialized.");
+        }
+    }
+}
