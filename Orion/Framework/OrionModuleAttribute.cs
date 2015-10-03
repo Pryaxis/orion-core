c467bedbf7da7cfb2f3bd69c499619ae2d28ad0c
Tyler Watson <tyler@tw.id.au>
2015-10-03 23:52:17 +1000

    bringing @WhiteXZ's framework over

---
+++ Orion/Framework/OrionModuleAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion
+{
+	public class OrionModuleAttribute : Attribute
+	{
+		public string ModuleName { get; private set; }
+		public string Author { get; private set; }
+		public int Order { get; private set; }
+
+		public OrionModuleAttribute(string name, string author, int order = 1)
+		{
+			ModuleName = name;
+			Author = author;
+			Order = order;
+		}
+	}
+}
---
+++ Orion/Framework/OrionModuleBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Orion
+{
+	public abstract class OrionModuleBase
+	{
+		public OrionModuleBase()
+		{
+			OrionModuleAttribute attrib = (OrionModuleAttribute)Attribute.GetCustomAttribute(GetType(), typeof(OrionModuleAttribute));
+
+			if (attrib == null)
+			{
+				throw new Exception("Module does not display OrionModuleAttribute");
+			}
+		}
+
+		public abstract void Run();
+	}
+}
