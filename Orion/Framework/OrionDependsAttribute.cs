4483ac82d199dcf7e1b03888d0e6349f33551c3b
Tyler Watson <tyler@tw.id.au>
2015-10-27 19:37:35 +1000

    OrionDepends framework
    Allows a module to explicitly specify that it depends on the
    presence of another module, and that module shall always be
    loaded and ready to go before the dependent is initialized.
    
    Signed-off-by: Tyler Watson <tyler@tw.id.au>

---
+++ Orion/Framework/OrionDependsAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Framework
+{
+    /// <summary>
+    /// Indicates that this module has a strict dependency on the availability of other modules.
+    /// Guarantees that by the time Initialize() gets called on the module, the module it depends on
+    /// shall be initialized.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class OrionDependsAttribute : Attribute
+    {
+        protected Type[] moduleDepends;
+
+        public OrionDependsAttribute(params Type[] moduleDepends)
+        {
+            this.moduleDepends = moduleDepends;
+        }
+
+    }
+}
