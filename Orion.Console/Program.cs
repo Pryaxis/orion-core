f43e80cea801b0d7da1ee3f4c9ae62d91fae071d
Tyler Watson <tyler.watson@ucg.com.au>
2016-07-13 17:10:20 +1000

    Basic Project skeleton which works

---
+++ Orion.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Orion;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        static Orion.Orion orion;
+
+        public static void Main(string[] args)
+        {
+            orion = new Orion.Orion(args);
+
+            orion.StartServer();
+        }
+    }
+}
---
+++ Orion/Orion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace Orion
+{
+    public class Orion
+    {
+
+        protected string[] CommandArguments { get; set; }
+
+        public Orion(params string[] args)
+        {
+            this.CommandArguments = args;
+        }
+
+        public void StartServer()
+        {
+            WindowsLaunch.Main(CommandArguments);
+        }
+    }
+}
