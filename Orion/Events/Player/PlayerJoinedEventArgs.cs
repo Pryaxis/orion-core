b1c5848f49cd952a86e9e2515193681a4a1d6b22
MarioE <marioe27@hotmail.com>
2016-07-17 15:46:20 -0400

    Document events, factor out entities.
    Added generic EntityEventArgs<TEntity>, HandledEntityEventArgs<TEntity>, and IEntityService<TEntity> to facilitate the creation of future entity-related services (such as items, projectiles, etc).

--- Orion/Events/Player/PlayerJoinedEventArgs.cs
+++ Orion/Events/Player/PlayerJoinedEventArgs.cs
@@ -1,7 +1,16 @@
-﻿namespace Orion.Events.Player
+﻿using Orion.Interfaces;
+
+namespace Orion.Events.Player
 {
-	public class PlayerJoinedEventArgs : PlayerEventArgs
+	/// <summary>
+	/// Provides data for the <see cref="IPlayerService.PlayerJoined"/> event.
+	/// </summary>
+	public class PlayerJoinedEventArgs : EntityEventArgs<Terraria.Player>
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PlayerJoinedEventArgs"/> class with the specified player.
+		/// </summary>
+		/// <param name="player">The player.</param>
 		public PlayerJoinedEventArgs(Terraria.Player player) : base(player)
 		{
 		}
