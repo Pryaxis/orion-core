f00fe6a13699f02b2d8673bc7ad016f2fde14f22
White <WhiteXZ@users.noreply.github.com>
2016-07-17 14:34:29 +0930

    Added NPCHandledEventArgs for NPC events that can be cancelled

---
+++ Orion/Events/NPCHandledEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Events
+{
+	public class NPCHandledEventArgs : HandledEventArgs
+	{
+		public Terraria.NPC NPC { get; set; }
+	}
+}
