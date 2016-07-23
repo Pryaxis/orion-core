8fef91a46ff82472026fba3be5efab5c117b65c0
Tyler Watson <tyler@tw.id.au>
2016-07-23 23:40:11 +1000

    Unmanaged dispose support on ServiceBase

--- Orion/Framework/ServiceBase.cs
+++ Orion/Framework/ServiceBase.cs
@@ -44,6 +44,23 @@ namespace Orion.Framework
 			}
 		}
 
+        /// <summary>
+        /// Destructor: ServiceBase
+        /// 
+        /// If your service has unmanaged resources, you must override <see cref="Dispose(bool)"/>
+        /// and release it.
+        /// </summary>
+        ~ServiceBase()
+        {
+            /*
+             * All unmanaged service resources must be deallocated in Dispose() regardless of if it's a
+             * managed dispose or a finalizer, HGlobal memory *must* be freed regardless or it will leak.
+             * 
+             * See CA1063 https://msdn.microsoft.com/en-us/library/ms244737.aspx
+             */
+            Dispose(false);
+        }
+
 		/// <summary>
 		/// Disposes the service, suppressing the GC from running the finalizer, if any.
 		/// </summary>
@@ -58,7 +75,8 @@ namespace Orion.Framework
 		/// any.
 		/// </summary>
 		/// <param name="disposing">
-		/// true to dispose managed and unmanaged resources, false to only dispose unmanaged resources.
+		/// true if Dispose() was called from a managed disposal, and *both* unmanaged and managed resources must be freed.
+        /// false if Dispose() was called from a finalizer, and *only* unmanaged resources may be freed.
 		/// </param>
 		protected virtual void Dispose(bool disposing)
 		{
