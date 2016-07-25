41461b0eff3219eca07c57b3f76716ff1e0f089e
Patrikk <patrikveres@hotmail.com>
2016-07-25 14:39:05 +0200

    Add NpcLootDrop events

---
+++ Orion/Events/Npc/NpcDroppedLootEventArgs.cs
@@ -0,0 +1,47 @@
+﻿using Orion.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Events.Npc
+{
+	/// <summary>
+	/// Provides data for the <see cref="IItemService.NpcDroppedLoot"/> event.
+	/// </summary>
+	public class NpcDroppedLootEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Gets the <see cref="IItem"/> that the NPC has dropped.
+		/// </summary>
+		public IItem Item { get; }
+
+		/// <summary>
+		/// Gets the <see cref="INpc"/> that dropped the item.
+		/// </summary>
+		public INpc Npc { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NpcDroppedLootEventArgs"/> class.
+		/// </summary>
+		/// <param name="item">The <see cref="IItem"/> that has dropped.</param>
+		/// <param name="npc">The <see cref="INpc"/> that has dropped the item.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="item"/> was null.</exception>
+		/// <exception cref="ArgumentNullException"><paramref name="npc"/> was null.</exception>
+		public NpcDroppedLootEventArgs(IItem item, INpc npc)
+		{
+			if (item == null)
+			{
+				throw new ArgumentNullException(nameof(item));
+			}
+			if (npc == null)
+			{
+				throw new ArgumentException(nameof(npc));
+			}
+
+			Item = item;
+			Npc = npc;
+		}
+	}
+}
---
+++ Orion/Events/Npc/NpcDroppingLootEventArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Orion.Interfaces;
+using System.ComponentModel;
+
+namespace Orion.Events.Npc
+{
+	/// <summary>
+	/// Provides data for the <see cref="IItemService.NpcDroppingLoot"/> event.
+	/// </summary>
+	public class NpcDroppingLootEventArgs : HandledEventArgs
+	{
+		/// <summary>
+		/// Gets the <see cref="IItem"/> that the NPC is dropping.
+		/// </summary>
+		public IItem Item { get; }
+
+		/// <summary>
+		/// Gets the <see cref="INpc"/> that is dropping the item.
+		/// </summary>
+		public INpc Npc { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NpcDroppingLootEventArgs"/> class.
+		/// </summary>
+		/// <param name="item">The <see cref="IItem"/> that is dropping.</param>
+		/// <param name="npc">The <see cref="INpc"/> that is dropping the item.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="item"/> or <paramref name="npc"/> was null.</exception>
+		public NpcDroppingLootEventArgs(IItem item, INpc npc)
+		{
+			if (item == null)
+			{
+				throw new ArgumentNullException(nameof(item));
+			}
+			if (npc == null)
+			{
+				throw new ArgumentException(nameof(npc));
+			}
+
+			Item = item;
+			Npc = npc;
+		}
+	}
+}
