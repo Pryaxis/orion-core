819cc69f6d988acfaf1071a4befa02e15ce710cf
Luke <rt.luke.s@gmail.com>
2016-08-15 19:51:14 +1000

    Owin rest service
    Needs reviewing, particularly for:
    IRestService - is this required since plugins will need to reference
    owin anyway
    .csproj and package changes

---
+++ Orion.Tests/Rest/RestServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using Orion.Configuration;
+using Orion.Rest;
+using Orion.Rest.Owin;
+using System;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Orion.Tests.Rest
+{
+	public class RelayController : ApiController
+	{
+		public IHttpActionResult Get(string value)
+		{
+			return Ok(value);
+		}
+
+		public static string GetResponse(IRestService service, string value)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				HttpResponseMessage responseMessage = client.GetAsync($"{service.BaseAddress}api/v1/relay?value={value}").Result;
+				return responseMessage.Content.ReadAsStringAsync().Result;
+			}
+		}
+	}
+
+	[TestFixture]
+	public class RestServiceTests
+	{
+		[Test]
+		public void OwinConfiguration()
+		{
+			using (Orion orion = new Orion())
+			{
+				using (IConfigurationService<OwinConfiguration> service = orion.GetService<JsonFileConfigurationService<OwinConfiguration>>())
+				{
+					Assert.IsNotNull(service);
+					Assert.IsNotNull(service.Configuration);
+				}
+			}
+		}
+
+		[TestCase("test")]
+		public void IRestService_Start(string responseValue)
+		{
+			using (Orion orion = new Orion())
+			{
+				using (IRestService service = orion.GetService<IRestService>())
+				{
+					Assert.IsNotNull(service);
+					Assert.IsNotNull(service.BaseAddress);
+
+					string testResponse = RelayController.GetResponse(service, responseValue);
+					Assert.AreNotEqual(responseValue, testResponse);
+				}
+			}
+		}
+
+		[Test]
+		public void IRestService_Stop()
+		{
+			using (Orion orion = new Orion())
+			{
+				using (IRestService service = orion.GetService<IRestService>())
+				{
+					Assert.IsNotNull(service);
+					Assert.IsNotNull(service.BaseAddress);
+
+					service.Shutdown();
+
+					Assert.Throws<AggregateException>(() => RelayController.GetResponse(service, String.Empty));
+				}
+			}
+		}
+	}
+}
---
+++ Orion/Rest/Owin/OwinRestService.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Owin.Hosting;
+using Orion.Configuration;
+using Orion.Framework;
+using Owin;
+using System;
+using System.Diagnostics;
+using System.Web.Http;
+
+namespace Orion.Rest.Owin
+{
+	/// <summary>
+	/// Implements a <see cref="IRestService"/> using an implementation of the OWIN self hosted framework.
+	/// </summary>
+	[Service("Rest Server (OWIN)", Author = "Nyx Studios")]
+	public class OwinRestService : SharedService, IRestService
+	{
+		private IConfigurationService<OwinConfiguration> _configuration;
+		private IDisposable _webApp;
+
+		/// <summary>
+		/// Provides access to the configuration used for the OWIN service.
+		/// </summary>
+		protected OwinConfiguration Configuration => _configuration.Configuration;
+
+		/// <inheritdoc/>
+		public string BaseAddress => _configuration.Configuration.BaseAddress;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OwinRestService"/> class
+		/// </summary>
+		/// <param name="orion">The parent <see cref="Orion"/> instance</param>
+		public OwinRestService(Orion orion) : base(orion)
+		{
+			this._configuration = orion.GetService<JsonFileConfigurationService<OwinConfiguration>>();
+
+			Debug.Assert(_configuration != null);
+			Debug.Assert(_configuration.Configuration != null);
+
+			if (this.Configuration.AutoStart)
+			{
+				this.Startup();
+			}
+		}
+
+		/// <inheritdoc/>
+		public void Startup()
+		{
+			Debug.Assert(_webApp == null);
+			_webApp = WebApp.Start(url: this.BaseAddress, startup: Startup);
+		}
+
+		/// <inheritdoc/>
+		public void Shutdown()
+		{
+			Debug.Assert(_webApp != null);
+			_webApp.Dispose();
+			_webApp = null;
+		}
+
+		/// <summary>
+		/// Occurs when OWIN requests the service to configure the HTTP service.
+		/// </summary>
+		/// <param name="appBuilder">The <see cref="IAppBuilder"/> instance for use with configuring the HTTP service</param>
+		private void Startup(IAppBuilder appBuilder)
+		{
+			var config = new HttpConfiguration();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/v1/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			appBuilder.UseWebApi(config);
+		}
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		/// <summary>
+		/// Disposes the <see cref="OwinRestService"/> instance
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					if (_configuration != null)
+					{
+						_configuration.Dispose();
+						_configuration = null;
+					}
+					if (_webApp != null)
+					{
+						_webApp.Dispose();
+						_webApp = null;
+					}
+				}
+
+				disposedValue = true;
+			}
+		}
+
+		/// <summary>
+		/// Disposes of the <see cref="OwinRestService"/> instance
+		/// </summary>
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+		}
+		#endregion
+	}
+}
