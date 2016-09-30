7b48a12aa9f99d14675324ba321563c08fb70dcd
White <WhiteXZ@users.noreply.github.com>
2016-09-30 14:55:51 +0930

    Added asynchronous operations to IGroup and IGroupService

--- Orion/Authorization/IGroupService.cs
+++ Orion/Authorization/IGroupService.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using Orion.Framework;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Orion.Authorization
 {
@@ -36,6 +37,18 @@ namespace Orion.Authorization
 		IEnumerable<IGroup> Find(Predicate<IGroup> predicate = null);
 
 		/// <summary>
+		/// Asynchronously finds a list of groups, optionally filtered by the supplied <paramref name="predicate"/>.
+		/// </summary>
+		/// <param name="predicate">
+		/// (optional) A predicate expression to filter groups by.
+		/// </param>
+		/// <returns>
+		/// An enumerable of all <see cref="IGroup"/> instances matching the specified <paramref name="predicate"/>.
+		/// If no predicate expression was specified, returns all groups.
+		/// </returns>
+		Task<IEnumerable<IGroup>> FindAsync(Predicate<IGroup> predicate = null);
+
+		/// <summary>
 		/// Adds a group with the specified group name to the service, optionally with an initial list of members.
 		/// </summary>
 		/// <param name="groupName">
@@ -47,10 +60,27 @@ namespace Orion.Authorization
 		IGroup AddGroup(string groupName, IEnumerable<IUserAccount> initialMembers = null);
 
 		/// <summary>
+		/// Asynchronously adds a group with the specified group name to the service, optionally with an initial list of members.
+		/// </summary>
+		/// <param name="groupName">
+		/// A string containing the group name to add to the service.
+		/// </param>
+		/// <param name="initialMembers">
+		/// (optional) A list of user accounts that will initially become members of this group.
+		/// </param>
+		Task<IGroup> AddGroupAsync(string groupName, IEnumerable<IUserAccount> initialMembers = null);
+
+		/// <summary>
 		/// Deletes the specified <paramref name="group"/>.
 		/// </summary>
 		/// <param name="group">A reference to the group to be deleted.</param>
 		void DeleteGroup(IGroup group);
+
+		/// <summary>
+		/// Asynchronously deletes the specified <paramref name="group"/>.
+		/// </summary>
+		/// <param name="group">A reference to the group to be deleted.</param>
+		Task DeleteGroupAsync(IGroup group);
 
 		/// <summary>
 		/// Binds the specified list of user accounts to the specified group.
@@ -60,5 +90,14 @@ namespace Orion.Authorization
 		/// </param>
 		/// <param name="userAccounts">A list of user account objects to add to the specified group.</param>
 		void AddMembers(IGroup group, params IUserAccount[] userAccounts);
+
+		/// <summary>
+		/// Asynchronously binds the specified list of user accounts to the specified group.
+		/// </summary>
+		/// <param name="group">
+		/// A reference to the group object to bind members to.
+		/// </param>
+		/// <param name="userAccounts">A list of user account objects to add to the specified group.</param>
+		Task AddMembersAsync(IGroup group, params IUserAccount[] userAccounts);
 	}
 }
