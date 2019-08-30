c2a952b9fe38e569a632471d57cb0acc67ae17a5
Kevin Zhao <kzhao1729@gmail.com>
2019-08-29 18:24:00 -0700

    Implement tests for Orion.Framework

---
+++ Orion.Tests/Framework/TypeExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace Orion.Tests.Framework {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
+    using Orion.Framework;
+    using Xunit;
+
+    public class TypeExtensionsTests {
+        [Fact]
+        public void GetAllSubtypes_Null_ThrowsArgumentNullException() {
+            Action action = () => ((Type)null).GetAllSubtypes();
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GetAllSubtypes_IsCorrect() {
+            var subtypes = typeof(BaseType).GetAllSubtypes().ToList();
+
+            subtypes.Should().HaveCount(2);
+            subtypes.Should().Contain(typeof(DerivedType));
+            subtypes.Should().Contain(typeof(DerivedType2));
+        }
+        
+        [Fact]
+        public void GetGenericTypeMaybe_Null_ThrowsArgumentNullException() {
+            Action action = () => ((Type)null).GetGenericTypeMaybe();
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void GetGenericTypeMaybe_NonGenericType_IsCorrect() {
+            var type = typeof(int).GetGenericTypeMaybe();
+
+            type.Should().Be<int>();
+        }
+        
+        [Fact]
+        public void GetGenericTypeMaybe_GenericType_IsCorrect() {
+            var type = typeof(List<int>).GetGenericTypeMaybe();
+
+            type.Should().Be(typeof(List<>));
+        }
+
+
+
+        public class BaseType {
+        }
+
+        public class DerivedType : BaseType {
+
+        }
+
+        public class DerivedType2 : BaseType {
+
+        }
+    }
+}
