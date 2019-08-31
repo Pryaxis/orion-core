ff5e0193337c7345d2a5814b405a4e8ee41841aa
Kevin Zhao <kzhao1729@gmail.com>
2019-08-31 03:49:49 -0700

    Add ITileService.

--- Orion.Tests/Framework/OrionNinjectModuleTests.cs
+++ Orion.Tests/Framework/OrionNinjectModuleTests.cs
@@ -49,7 +49,6 @@
         }
 
 
-
         public interface IMockService : IService {
         }
 
---
+++ Orion/World/ITileService.cs
@@ -0,0 +1,10 @@
+﻿using Orion.Framework;
+using OTAPI.Tile;
+
+namespace Orion.World {
+    /// <summary>
+    /// Provides access to Terraria's tile mechanism.
+    /// </summary>
+    public interface ITileService : IService, ITileCollection {
+    }
+}
