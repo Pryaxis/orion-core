a5a2e07753b8dae61da40f76ff9da557d4d3f7b0
Kevin Zhao <kzhao1729@gmail.com>
2019-08-31 03:55:56 -0700

    Add Entities.

---
+++ Orion/Entities/IEntity.cs
@@ -0,0 +1,33 @@
+﻿namespace Orion.Entities {
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// Provides a wrapper around a <see cref="Terraria.Entity"/>.
+    /// </summary>
+    public interface IEntity {
+        /// <summary>
+        /// Gets or sets the entity's height in pixels.
+        /// </summary>
+        int Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity's position in the world.
+        /// </summary>
+        Vector2 Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity's velocity in the world.
+        /// </summary>
+        Vector2 Velocity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity's width in pixels.
+        /// </summary>
+        int Width { get; set; }
+
+        /// <summary>
+        /// Gets the wrapped <see cref="Terraria.Entity"/> instance.
+        /// </summary>
+        Terraria.Entity WrappedEntity { get; }
+    }
+}
---
+++ Orion/Entities/OrionEntity.cs
@@ -0,0 +1,46 @@
+﻿namespace Orion.Entities {
+    using System;
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// Orion's implementation of <see cref="IEntity"/>.
+    /// </summary>
+    internal class OrionEntity : IEntity {
+        /// <inheritdoc/>
+        public int Height {
+            get => WrappedEntity.height;
+            set => WrappedEntity.height = value;
+        }
+
+        /// <inheritdoc/>
+        public Vector2 Position {
+            get => WrappedEntity.position;
+            set => WrappedEntity.position = value;
+        }
+
+        /// <inheritdoc/>
+        public Vector2 Velocity {
+            get => WrappedEntity.velocity;
+            set => WrappedEntity.velocity = value;
+        }
+
+        /// <inheritdoc/>
+        public int Width {
+            get => WrappedEntity.width;
+            set => WrappedEntity.width = value;
+        }
+
+        /// <inheritdoc/>
+        public Terraria.Entity WrappedEntity { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrionEntity"/> class wrapping the specified Terraria Entity
+        /// instance.
+        /// </summary>
+        /// <param name="entity">The Terraria Entity instance to wrap.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="entity"/> is null.</exception>
+        public OrionEntity(Terraria.Entity entity) {
+            WrappedEntity = entity ?? throw new ArgumentNullException(nameof(entity));
+        }
+    }
+}
