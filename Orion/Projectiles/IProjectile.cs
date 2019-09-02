22c5014adc60859d82a7f9318ad692f289f82faf
Kevin Zhao <kzhao1729@gmail.com>
2019-09-02 00:59:46 -0700

    Begin Projectiles.

---
+++ Orion/Projectiles/IProjectile.cs
@@ -0,0 +1,18 @@
+﻿using Orion.Entities;
+
+namespace Orion.Projectiles {
+    /// <summary>
+    /// Provides a wrapper around a Terraria.Projectile.
+    /// </summary>
+    public interface IProjectile : IEntity {
+        /// <summary>
+        /// Gets or sets the projectile type.
+        /// </summary>
+        ProjectileType Type { get; set; }
+
+        /// <summary>
+        /// Gets the wrapped Terraria Projectile instance.
+        /// </summary>
+        Terraria.Projectile WrappedProjectile { get; }
+    }
+}
