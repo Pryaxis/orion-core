4fd539ca5a7e9ab29b3b27ea5679ac847ff49635
Kevin Zhao <kzhao1729@gmail.com>
2019-09-02 00:59:40 -0700

    Begin NPCs.

---
+++ Orion/Npcs/INpc.cs
@@ -0,0 +1,18 @@
+﻿using Orion.Entities;
+
+namespace Orion.Npcs {
+    /// <summary>
+    /// Provides a wrapper arround a Terraria.NPC instance.
+    /// </summary>
+    public interface INpc : IEntity {
+        /// <summary>
+        /// Gets or sets the NPC's type.
+        /// </summary>
+        NpcType Type { get; set; }
+
+        /// <summary>
+        /// Gets the wrapped Terraria NPC instance.
+        /// </summary>
+        Terraria.NPC WrappedNpc { get; }
+    }
+}
