e87641fabe3966a781683341ec4609a96ec852d1
Kevin Zhao <kzhao1729@gmail.com>
2019-09-04 00:42:41 -0700

    Add PaintType.

---
+++ Orion/World/PaintType.cs
@@ -0,0 +1,40 @@
+﻿namespace Orion.World {
+    /// <summary>
+    /// Specifies the type of paint.
+    /// </summary>
+    public enum PaintType {
+#pragma warning disable 1591
+        Blank = 0,
+        Red = 1,
+        Orange = 2,
+        Yellow = 3,
+        Lime = 4,
+        Green = 5,
+        Teal = 6,
+        Cyan = 7,
+        SkyBlue = 8,
+        Blue = 9,
+        Purple = 10,
+        Violet = 11,
+        Pink = 12,
+        DeepRed = 13,
+        DeepOrange = 14,
+        DeepYellow = 15,
+        DeepLime = 16,
+        DeepGreen = 17,
+        DeepTeal = 18,
+        DeepCyan = 19,
+        DeepSkyBlue = 20,
+        DeepBlue = 21,
+        DeepPurple = 22,
+        DeepViolet = 23,
+        DeepPink = 24,
+        Black = 25,
+        White = 26,
+        Gray = 27,
+        Brown = 28,
+        Shadow = 29,
+        Negative = 30,
+#pragma warning restore 1591
+    }
+}
