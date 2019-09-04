3a475bd75f8650117a1c07862ab5038881566c82
Kevin Zhao <kzhao1729@gmail.com>
2019-09-03 21:01:02 -0700

    Add some more IPlayer properties.

--- Orion/Players/IPlayer.cs
+++ Orion/Players/IPlayer.cs
@@ -4,5 +4,25 @@ namespace Orion.Players {
     /// <summary>
     /// Provides a wrapper around a Terraria.Player instance.
     /// </summary>
-    public interface IPlayer : IEntity { }
+    public interface IPlayer : IEntity {
+        /// <summary>
+        /// Gets or sets the player's HP.
+        /// </summary>
+        int Hp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player's max HP.
+        /// </summary>
+        int MaxHp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player's MP.
+        /// </summary>
+        int Mp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player's max MP.
+        /// </summary>
+        int MaxMp { get; set; }
+    }
 }
--- Orion/Players/OrionPlayer.cs
+++ Orion/Players/OrionPlayer.cs
@@ -6,6 +6,26 @@ namespace Orion.Players {
     /// Orion's implementation of <see cref="IPlayer"/>.
     /// </summary>
     internal sealed class OrionPlayer : OrionEntity, IPlayer {
+        public int Hp {
+            get => WrappedPlayer.statLife;
+            set => WrappedPlayer.statLife = value;
+        }
+
+        public int MaxHp {
+            get => WrappedPlayer.statLifeMax;
+            set => WrappedPlayer.statLifeMax = value;
+        }
+
+        public int Mp {
+            get => WrappedPlayer.statMana;
+            set => WrappedPlayer.statMana = value;
+        }
+
+        public int MaxMp {
+            get => WrappedPlayer.statManaMax;
+            set => WrappedPlayer.statManaMax = value;
+        }
+
         internal Terraria.Player WrappedPlayer { get; }
 
         public OrionPlayer(Terraria.Player terrariaPlayer) : base(terrariaPlayer) {
