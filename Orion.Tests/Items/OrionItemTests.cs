4c260d7ec789adf53ff2afc70ba091ae549ad0e9
Kevin Zhao <kzhao1729@gmail.com>
2019-09-07 02:01:09 -0700

    Add unit tests for OrionItem and OrionNpc.

--- Orion.Tests/Items/OrionItemTests.cs
+++ Orion.Tests/Items/OrionItemTests.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using FluentAssertions;
+using Microsoft.Xna.Framework;
 using Orion.Items;
 using Xunit;
 
 namespace Orion.Tests.Items {
     public class OrionItemTests {
+        [Theory]
+        [InlineData(100)]
+        public void GetIndex_IsCorrect(int index) {
+            var terrariaItem = new Terraria.Item {whoAmI = index};
+            var item = new OrionItem(terrariaItem);
+
+            item.Index.Should().Be(index);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void GetIsActive_IsCorrect(bool isActive) {
+            var terrariaItem = new Terraria.Item {active = isActive};
+            var item = new OrionItem(terrariaItem);
+
+            item.IsActive.Should().Be(isActive);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void SetIsActive_IsCorrect(bool isActive) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.IsActive = isActive;
+
+            terrariaItem.active.Should().Be(isActive);
+        }
+
+        [Theory]
+        [InlineData("test")]
+        public void GetName_IsCorrect(string name) {
+            var terrariaItem = new Terraria.Item {_nameOverride = name};
+            var item = new OrionItem(terrariaItem);
+
+            item.Name.Should().Be(name);
+        }
+
+        [Theory]
+        [InlineData("test")]
+        public void SetName_IsCorrect(string name) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.Name = name;
+
+            terrariaItem.Name.Should().Be(name);
+        }
+
         [Fact]
         public void SetName_NullValue_ThrowsArgumentNullException() {
             var terrariaItem = new Terraria.Item();
@@ -12,6 +64,202 @@ namespace Orion.Tests.Items {
             Action action = () => item.Name = null;
 
             action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GetPosition_IsCorrect() {
+            var terrariaItem = new Terraria.Item {position = new Vector2(100, 100)};
+            var item = new OrionItem(terrariaItem);
+
+            item.Position.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void SetPosition_IsCorrect() {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.Position = new Vector2(100, 100);
+
+            terrariaItem.position.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void GetVelocity_IsCorrect() {
+            var terrariaItem = new Terraria.Item {velocity = new Vector2(100, 100)};
+            var item = new OrionItem(terrariaItem);
+
+            item.Velocity.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void SetVelocity_IsCorrect() {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.Velocity = new Vector2(100, 100);
+
+            terrariaItem.velocity.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void GetSize_IsCorrect() {
+            var terrariaItem = new Terraria.Item {Size = new Vector2(100, 100)};
+            var item = new OrionItem(terrariaItem);
+
+            item.Size.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void SetSize_IsCorrect() {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.Size = new Vector2(100, 100);
+
+            terrariaItem.Size.Should().Be(new Vector2(100, 100));
+        }
+
+        [Theory]
+        [InlineData(ItemType.SDMG)]
+        public void GetType_IsCorrect(ItemType itemType) {
+            var terrariaItem = new Terraria.Item {type = (int)itemType};
+            var item = new OrionItem(terrariaItem);
+
+            item.Type.Should().Be(itemType);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void GetStackSize_IsCorrect(int stackSize) {
+            var terrariaItem = new Terraria.Item {stack = stackSize};
+            var item = new OrionItem(terrariaItem);
+
+            item.StackSize.Should().Be(stackSize);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void SetStackSize_IsCorrect(int stackSize) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.StackSize = stackSize;
+
+            terrariaItem.stack.Should().Be(stackSize);
+        }
+
+        [Theory]
+        [InlineData(ItemPrefix.Unreal)]
+        public void GetPrefix_IsCorrect(ItemPrefix itemPrefix) {
+            var terrariaItem = new Terraria.Item {prefix = (byte)itemPrefix};
+            var item = new OrionItem(terrariaItem);
+
+            item.Prefix.Should().Be(itemPrefix);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void GetMaxStackSize_IsCorrect(int maxStackSize) {
+            var terrariaItem = new Terraria.Item {maxStack = maxStackSize};
+            var item = new OrionItem(terrariaItem);
+
+            item.MaxStackSize.Should().Be(maxStackSize);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void SetMaxStackSize_IsCorrect(int maxStackSize) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.MaxStackSize = maxStackSize;
+
+            terrariaItem.maxStack.Should().Be(maxStackSize);
+        }
+
+        [Theory]
+        [InlineData(ItemRarity.Orange)]
+        public void GetRarity_IsCorrect(ItemRarity itemRarity) {
+            var terrariaItem = new Terraria.Item {rare = (int)itemRarity};
+            var item = new OrionItem(terrariaItem);
+
+            item.Rarity.Should().Be(itemRarity);
+        }
+
+        [Theory]
+        [InlineData(ItemRarity.Orange)]
+        public void SetRarity_IsCorrect(ItemRarity itemRarity) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.Rarity = itemRarity;
+
+            terrariaItem.rare.Should().Be((int)itemRarity);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void GetDamage_IsCorrect(int damage) {
+            var terrariaItem = new Terraria.Item {damage = damage};
+            var item = new OrionItem(terrariaItem);
+
+            item.Damage.Should().Be(damage);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void SetDamage_IsCorrect(int damage) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.Damage = damage;
+
+            terrariaItem.damage.Should().Be(damage);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void GetUseTime_IsCorrect(int useTime) {
+            var terrariaItem = new Terraria.Item {useTime = useTime};
+            var item = new OrionItem(terrariaItem);
+
+            item.UseTime.Should().Be(useTime);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void SetUseTime_IsCorrect(int useTime) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.UseTime = useTime;
+
+            terrariaItem.useTime.Should().Be(useTime);
+        }
+        
+
+        [Theory]
+        [InlineData(ItemType.SDMG)]
+        public void ApplyType_IsCorrect(ItemType itemType) {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.ApplyType(itemType);
+
+            terrariaItem.type.Should().Be((int)itemType);
+        }
+        
+        [Theory]
+        [InlineData(ItemPrefix.Unreal)]
+        public void ApplyPrefix_IsCorrect(ItemPrefix itemPrefix) {
+            var terrariaItem = new Terraria.Item();
+            terrariaItem.SetDefaults((int)ItemType.SDMG);
+            var item = new OrionItem(terrariaItem);
+
+            item.ApplyPrefix(itemPrefix);
+
+            terrariaItem.prefix.Should().Be((byte)itemPrefix);
         }
     }
 }
--- Orion.Tests/Npcs/OrionNpcTests.cs
+++ Orion.Tests/Npcs/OrionNpcTests.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using FluentAssertions;
+using Microsoft.Xna.Framework;
 using Orion.Npcs;
 using Xunit;
 
 namespace Orion.Tests.Npcs {
     public class OrionNpcTests {
+        [Theory]
+        [InlineData(100)]
+        public void GetIndex_IsCorrect(int index) {
+            var terrariaNpc = new Terraria.NPC {whoAmI = index};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Index.Should().Be(index);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void GetIsActive_IsCorrect(bool isActive) {
+            var terrariaNpc = new Terraria.NPC {active = isActive};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.IsActive.Should().Be(isActive);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void SetIsActive_IsCorrect(bool isActive) {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.IsActive = isActive;
+
+            terrariaNpc.active.Should().Be(isActive);
+        }
+
+        [Theory]
+        [InlineData("test")]
+        public void GetName_IsCorrect(string name) {
+            var terrariaNpc = new Terraria.NPC {_givenName = name};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Name.Should().Be(name);
+        }
+
+        [Theory]
+        [InlineData("test")]
+        public void SetName_IsCorrect(string name) {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Name = name;
+
+            terrariaNpc.GivenOrTypeName.Should().Be(name);
+        }
+
         [Fact]
         public void SetName_NullValue_ThrowsArgumentNullException() {
             var terrariaNpc = new Terraria.NPC();
@@ -12,6 +64,138 @@ namespace Orion.Tests.Npcs {
             Action action = () => npc.Name = null;
 
             action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GetPosition_IsCorrect() {
+            var terrariaNpc = new Terraria.NPC {position = new Vector2(100, 100)};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Position.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void SetPosition_IsCorrect() {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Position = new Vector2(100, 100);
+
+            terrariaNpc.position.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void GetVelocity_IsCorrect() {
+            var terrariaNpc = new Terraria.NPC {velocity = new Vector2(100, 100)};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Velocity.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void SetVelocity_IsCorrect() {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Velocity = new Vector2(100, 100);
+
+            terrariaNpc.velocity.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void GetSize_IsCorrect() {
+            var terrariaNpc = new Terraria.NPC {Size = new Vector2(100, 100)};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Size.Should().Be(new Vector2(100, 100));
+        }
+
+        [Fact]
+        public void SetSize_IsCorrect() {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Size = new Vector2(100, 100);
+
+            terrariaNpc.Size.Should().Be(new Vector2(100, 100));
+        }
+        
+        [Theory]
+        [InlineData(NpcType.BlueSlime)]
+        public void GetType_IsCorrect(NpcType npcType) {
+            var terrariaNpc = new Terraria.NPC {netID = (int)npcType};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Type.Should().Be(npcType);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void GetHp_IsCorrect(int hp) {
+            var terrariaNpc = new Terraria.NPC {life = hp};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Hp.Should().Be(hp);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void SetHp_IsCorrect(int hp) {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Hp = hp;
+
+            terrariaNpc.life.Should().Be(hp);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void GetMaxHp_IsCorrect(int maxMaxHp) {
+            var terrariaNpc = new Terraria.NPC {lifeMax = maxMaxHp};
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.MaxHp.Should().Be(maxMaxHp);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void SetMaxHp_IsCorrect(int maxMaxHp) {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.MaxHp = maxMaxHp;
+
+            terrariaNpc.lifeMax.Should().Be(maxMaxHp);
+        }
+
+        [Theory]
+        [InlineData(NpcType.BlueSlime)]
+        public void ApplyType_IsCorrect(NpcType npcType) {
+            var terrariaNpc = new Terraria.NPC();
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.ApplyType(npcType);
+
+            terrariaNpc.netID.Should().Be((int)npcType);
+        }
+
+        [Theory]
+        [InlineData(NpcType.BlueSlime, NpcType.GreenSlime)]
+        [InlineData(NpcType.BlueSlime, NpcType.None)]
+        public void Transform_IsCorrect(NpcType oldType, NpcType newType) {
+            // TargetClosest requires non-null player array.
+            for (var i = 0; i < Terraria.Main.maxPlayers; ++i) {
+                Terraria.Main.player[i] = new Terraria.Player();
+            }
+
+            var terrariaNpc = new Terraria.NPC();
+            terrariaNpc.SetDefaults((int)oldType);
+            var npc = new OrionNpc(terrariaNpc);
+
+            npc.Transform(newType);
+
+            terrariaNpc.netID.Should().Be((int)newType);
         }
     }
 }
