8aa17d23337b90ae908af2c9950c3ec37eb0cef9
Kevin Zhao <kzhao1729@gmail.com>
2019-09-15 19:05:56 -0700

    Reintroduce PlayerStealthPacket.

---
+++ src/Orion/Networking/Packets/Players/PlayerStealthPacket.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace Orion.Networking.Packets.Players {
+    /// <summary>
+    /// Packet sent to set a player's stealth.
+    /// </summary>
+    public sealed class PlayerStealthPacket : Packet {
+        private byte _playerIndex;
+        private float _playerStealthStatus;
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        public byte PlayerIndex {
+            get => _playerIndex;
+            set {
+                _playerIndex = value;
+                IsDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the player's stealth.
+        /// </summary>
+        public float PlayerStealthStatus {
+            get => _playerStealthStatus;
+            set {
+                _playerStealthStatus = value;
+                IsDirty = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public override PacketType Type => PacketType.PlayerStealth;
+
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        public override string ToString() => $"{Type}[#={PlayerIndex}, ...]";
+
+        private protected override void ReadFromReader(BinaryReader reader, PacketContext context) {
+            _playerIndex = reader.ReadByte();
+            _playerStealthStatus = reader.ReadSingle();
+        }
+
+        private protected override void WriteToWriter(BinaryWriter writer, PacketContext context) {
+            writer.Write(PlayerIndex);
+            writer.Write(PlayerStealthStatus);
+        }
+    }
+}
