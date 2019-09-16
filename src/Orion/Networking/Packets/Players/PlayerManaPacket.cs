f300b32f90f33e29a2871cb2c53fc6ad014ca84f
Kevin Zhao <kzhao1729@gmail.com>
2019-09-15 18:56:40 -0700

    Reintroduce PlayerManaPacket.

---
+++ src/Orion/Networking/Packets/Players/PlayerManaPacket.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace Orion.Networking.Packets.Players {
+    /// <summary>
+    /// Packet sent to set a player's mana and maximum mana.
+    /// </summary>
+    public sealed class PlayerManaPacket : Packet {
+        private byte _playerIndex;
+        private short _playerMana;
+        private short _playerMaxMana;
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        public byte PlayerIndex {
+            get => _playerIndex;
+            set {
+                _playerIndex = value;
+                IsDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the player's mana.
+        /// </summary>
+        public short PlayerMana {
+            get => _playerMana;
+            set {
+                _playerMana = value;
+                IsDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the player's maximum mana.
+        /// </summary>
+        public short PlayerMaxMana {
+            get => _playerMaxMana;
+            set {
+                _playerMaxMana = value;
+                IsDirty = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public override PacketType Type => PacketType.PlayerMana;
+
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        public override string ToString() => $"{Type}[#={PlayerIndex} has {PlayerMana}/{PlayerMaxMana} mp]";
+
+        private protected override void ReadFromReader(BinaryReader reader, PacketContext context) {
+            _playerIndex = reader.ReadByte();
+            _playerMana = reader.ReadInt16();
+            _playerMaxMana = reader.ReadInt16();
+        }
+
+        private protected override void WriteToWriter(BinaryWriter writer, PacketContext context) {
+            writer.Write(PlayerIndex);
+            writer.Write(PlayerMana);
+            writer.Write(PlayerMaxMana);
+        }
+    }
+}
