7430c08f828cde842236a78ff2227f6e44138b48
Kevin Zhao <kzhao1729@gmail.com>
2019-09-17 00:30:06 -0700

    Reintroduce PaintBlockPacket.

---
+++ src/Orion/Networking/Packets/World/PaintBlockPacket.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Orion.World;
+
+namespace Orion.Networking.Packets.World {
+    /// <summary>
+    /// Packet sent to paint a block.
+    /// </summary>
+    public sealed class PaintBlockPacket : Packet {
+        private short _blockX;
+        private short _blockY;
+        private PaintColor _blockColor;
+
+        /// <summary>
+        /// Gets or sets the block's X coordinate.
+        /// </summary>
+        public short BlockX {
+            get => _blockX;
+            set {
+                _blockX = value;
+                IsDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the block's Y coordinate.
+        /// </summary>
+        public short BlockY {
+            get => _blockY;
+            set {
+                _blockY = value;
+                IsDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the block color.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <c>null</c>.</exception>
+        public PaintColor BlockColor {
+            get => _blockColor;
+            set {
+                _blockColor = value ?? throw new ArgumentNullException(nameof(value));
+                IsDirty = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public override PacketType Type => PacketType.PaintBlock;
+
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        public override string ToString() => $"{Type}[{BlockColor} @ ({BlockX}, {BlockY})]";
+
+        private protected override void ReadFromReader(BinaryReader reader, PacketContext context) {
+            _blockX = reader.ReadInt16();
+            _blockY = reader.ReadInt16();
+            _blockColor = PaintColor.FromId(reader.ReadByte()) ?? throw new PacketException("Paint color is invalid.");
+        }
+
+        private protected override void WriteToWriter(BinaryWriter writer, PacketContext context) {
+            writer.Write(BlockX);
+            writer.Write(BlockY);
+            writer.Write(BlockColor.Id);
+        }
+    }
+}
---
+++ tests/Orion.Tests/Networking/Packets/World/PaintBlockPacketTests.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using FluentAssertions;
+using Orion.World;
+using Xunit;
+
+namespace Orion.Networking.Packets.World {
+    public class PaintBlockPacketTests {
+        [Fact]
+        public void SetBlockColor_NullValue_ThrowsArgumentNullException() {
+            var packet = new PaintBlockPacket();
+            Action action = () => packet.BlockColor = null;
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        public static readonly byte[] Bytes = {8, 0, 63, 0, 1, 100, 0, 1};
+
+        [Fact]
+        public void ReadFromStream_IsCorrect() {
+            using (var stream = new MemoryStream(Bytes)) {
+                var packet = (PaintBlockPacket)Packet.ReadFromStream(stream, PacketContext.Server);
+
+                packet.BlockX.Should().Be(256);
+                packet.BlockY.Should().Be(100);
+                packet.BlockColor.Should().BeSameAs(PaintColor.Red);
+            }
+        }
+
+        [Fact]
+        public void WriteToStream_IsCorrect() {
+            TestUtils.WriteToStream_SameBytes(Bytes);
+        }
+    }
+}
