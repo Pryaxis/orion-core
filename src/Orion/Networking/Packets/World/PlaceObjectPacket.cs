5e62e159af9309138ae595d3a1be3a160b450270
Kevin Zhao <kzhao1729@gmail.com>
2019-09-17 20:11:03 -0700

    Reintroduce PlaceObjectPacket.

---
+++ src/Orion/Networking/Packets/World/PlaceObjectPacket.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Orion.World;
+
+namespace Orion.Networking.Packets.World {
+    /// <summary>
+    /// Packet sent to place an object.
+    /// </summary>
+    public sealed class PlaceObjectPacket : Packet {
+        private short _objectX;
+        private short _objectY;
+        private BlockType _objectType;
+        private short _objectStyle;
+        private sbyte _objectRandomState;
+        private bool _objectDirection;
+
+        /// <inheritdoc />
+        public override PacketType Type => PacketType.PlaceObject;
+
+        /// <summary>
+        /// Gets or sets the object's X coordinate.
+        /// </summary>
+        public short ObjectX {
+            get => _objectX;
+            set {
+                _objectX = value;
+                _isDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the object's Y coordinate.
+        /// </summary>
+        public short ObjectY {
+            get => _objectY;
+            set {
+                _objectY = value;
+                _isDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the object type.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <c>null</c>.</exception>
+        public BlockType ObjectType {
+            get => _objectType;
+            set {
+                _objectType = value ?? throw new ArgumentNullException(nameof(value));
+                _isDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the object style.
+        /// </summary>
+        public short ObjectStyle {
+            get => _objectStyle;
+            set {
+                _objectStyle = value;
+                _isDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the object's random state.
+        /// </summary>
+        public sbyte ObjectRandomState {
+            get => _objectRandomState;
+            set {
+                _objectRandomState = value;
+                _isDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating the object direction.
+        /// </summary>
+        public bool ObjectDirection {
+            get => _objectDirection;
+            set {
+                _objectDirection = value;
+                _isDirty = true;
+            }
+        }
+
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        public override string ToString() => $"{Type}[{ObjectType}_{ObjectStyle} @ ({ObjectX}, {ObjectY}), ...]";
+
+        /// <inheritdoc />
+        private protected override void ReadFromReader(BinaryReader reader, PacketContext context) {
+            ObjectX = reader.ReadInt16();
+            ObjectY = reader.ReadInt16();
+            ObjectType = BlockType.FromId(reader.ReadUInt16()) ?? throw new PacketException("Object type is invalid.");
+            ObjectStyle = reader.ReadInt16();
+            reader.ReadByte();
+            ObjectRandomState = reader.ReadSByte();
+            ObjectDirection = reader.ReadBoolean();
+        }
+
+        /// <inheritdoc />
+        private protected override void WriteToWriter(BinaryWriter writer, PacketContext context) {
+            writer.Write(ObjectX);
+            writer.Write(ObjectY);
+            writer.Write(ObjectType.Id);
+            writer.Write(ObjectStyle);
+            writer.Write((byte)0);
+            writer.Write(ObjectRandomState);
+            writer.Write(ObjectDirection);
+        }
+    }
+}
---
+++ tests/Orion.Tests/Networking/Packets/World/PlaceObjectPacketTests.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using FluentAssertions;
+using Orion.World;
+using Xunit;
+
+namespace Orion.Networking.Packets.World {
+    public class PlaceObjectPacketTests {
+        [Fact]
+        public void SetObjectType_NullValue_ThrowsArgumentNullException() {
+            var packet = new PlaceObjectPacket();
+            Action action = () => packet.ObjectType = null;
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        public static readonly byte[] Bytes = {14, 0, 79, 0, 1, 100, 0, 21, 0, 1, 0, 0, 255, 1};
+
+        [Fact]
+        public void ReadFromStream_IsCorrect() {
+            using (var stream = new MemoryStream(Bytes)) {
+                var packet = (PlaceObjectPacket)Packet.ReadFromStream(stream, PacketContext.Server);
+
+                packet.ObjectX.Should().Be(256);
+                packet.ObjectY.Should().Be(100);
+                packet.ObjectType.Should().BeSameAs(BlockType.Chests);
+                packet.ObjectStyle.Should().Be(1);
+                packet.ObjectRandomState.Should().Be(-1);
+                packet.ObjectDirection.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public void WriteToStream_IsCorrect() {
+            TestUtils.WriteToStream_SameBytes(Bytes);
+        }
+    }
+}
