65288f17b953e78b2580746000ff678ca28396d7
Kevin Zhao <kzhao1729@gmail.com>
2019-09-20 18:32:33 -0700

    Add DirtiableArray<T>.

---
+++ src/Orion/Utils/DirtiableArray.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Orion.Events;
+
+namespace Orion.Utils {
+    /// <summary>
+    /// Represents a dirtiable array of objects.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class DirtiableArray<T> : IArray<T>, IDirtiable {
+        private readonly T[] _array;
+        private bool _isDirty;
+
+        private static bool ContainsDirtiableElements => typeof(IDirtiable).IsAssignableFrom(typeof(T));
+
+        /// <inheritdoc />
+        public int Count => _array.Length;
+
+        /// <inheritdoc cref="IArray{T}.this" />
+        public T this[int index] {
+            get => _array[index];
+            set {
+                _array[index] = value;
+                _isDirty = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public bool IsDirty =>
+            _isDirty || ContainsDirtiableElements && this.Cast<IDirtiable>().Any(d => d?.IsDirty == true);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DirtiableArray{T}"/> class with the specified count.
+        /// </summary>
+        /// <param name="count">The count.</param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="count"/> is negative.</exception>
+        public DirtiableArray(int count) {
+            if (count <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(count), "Count must be non-negative.");
+            }
+
+            _array = new T[count];
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)_array).GetEnumerator();
+
+        /// <inheritdoc />
+        [ExcludeFromCodeCoverage]
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <inheritdoc />
+        public void Clean() {
+            _isDirty = false;
+            if (!ContainsDirtiableElements) return;
+
+            foreach (var dirtiable in this.Cast<IDirtiable>()) {
+                dirtiable?.Clean();
+            }
+        }
+    }
+}
---
+++ tests/Orion.Tests/Utils/DirtiableArrayTests.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using FluentAssertions;
+using Moq;
+using Orion.Events;
+using Xunit;
+
+namespace Orion.Utils {
+    public class DirtiableArrayTests {
+        [Fact]
+        public void Ctor_NegativeCount_ThrowsArgumentOutOfRangeException() {
+            Func<DirtiableArray<int>> func = () => new DirtiableArray<int>(-1);
+
+            func.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void TIsIDirtiable_IsCorrect() {
+            var isDirty = true;
+            var mockDirtiable = new Mock<IDirtiable>();
+            mockDirtiable.Setup(d => d.IsDirty).Returns(() => isDirty);
+            mockDirtiable.Setup(d => d.Clean()).Callback(() => isDirty = false);
+            var array = new DirtiableArray<IDirtiable>(1);
+            array[0] = mockDirtiable.Object;
+
+            array.ShouldBeDirty();
+
+            mockDirtiable.VerifyGet(d => d.IsDirty);
+            mockDirtiable.Verify(d => d.Clean());
+            mockDirtiable.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void Count_IsCorrect() {
+            var array = new DirtiableArray<int>(100);
+
+            array.Count.Should().Be(100);
+        }
+
+        [Fact]
+        public void GetItem_IsCorrect() {
+            var array = new DirtiableArray<int>(1);
+
+            array[0].Should().Be(0);
+        }
+
+        [Fact]
+        public void SetItem_MarksAsDirty() {
+            var array = new DirtiableArray<int>(1);
+
+            array[0] = 100;
+
+            array.ShouldBeDirty();
+        }
+
+        [Fact]
+        public void GetEnumerator_IsCorrect() {
+            var array = new DirtiableArray<int>(1);
+            array[0] = 1;
+
+            array.Should().BeEquivalentTo(1);
+        }
+    }
+}
