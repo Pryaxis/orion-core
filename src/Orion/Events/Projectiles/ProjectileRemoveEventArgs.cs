69c84e699b114f08c16547c65e0932bfef39ba2e
Kevin Zhao <kzhao1729@gmail.com>
2019-09-25 20:43:31 -0700

    Reintroduce projectile-related events.

---
+++ src/Orion/Events/Projectiles/ProjectileRemoveEventArgs.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2015-2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using Orion.Projectiles;
+
+namespace Orion.Events.Projectiles {
+    /// <summary>
+    /// Provides data for the <see cref="IProjectileService.ProjectileRemove"/> event.
+    /// </summary>
+    public sealed class ProjectileRemoveEventArgs : ProjectileEventArgs, ICancelable {
+        /// <inheritdoc />
+        public bool IsCanceled { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectileRemoveEventArgs"/> class with the specified
+        /// projectile.
+        /// </summary>
+        /// <param name="projectile">The projectile.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="projectile"/> is <c>null</c>.</exception>
+        public ProjectileRemoveEventArgs(IProjectile projectile) : base(projectile) { }
+    }
+}
