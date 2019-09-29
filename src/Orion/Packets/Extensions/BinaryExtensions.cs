e80f96bc3a6c1b40a4be18bf835139318e509ea8
Kevin Zhao <kzhao1729@gmail.com>
2019-09-29 01:43:37 -0700

    Add assertions to BinaryExtensions.

--- src/Orion/Packets/Extensions/BinaryExtensions.cs
+++ src/Orion/Packets/Extensions/BinaryExtensions.cs
@@ -15,26 +15,41 @@
 // You should have received a copy of the GNU General Public License
 // along with Orion.  If not, see <https://www.gnu.org/licenses/>.
 
+using System.Diagnostics;
 using System.IO;
 using Microsoft.Xna.Framework;
 using TerrariaNetworkText = Terraria.Localization.NetworkText;
 
 namespace Orion.Packets.Extensions {
     internal static class BinaryExtensions {
-        public static Color ReadColor(this BinaryReader reader) =>
-            new Color(reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+        public static Color ReadColor(this BinaryReader reader) {
+            Debug.Assert(reader != null, "reader != null");
+
+            return new Color(reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+        }
 
 
-        public static TerrariaNetworkText ReadNetworkText(this BinaryReader reader) =>
-            TerrariaNetworkText.Deserialize(reader);
+        public static TerrariaNetworkText ReadNetworkText(this BinaryReader reader) {
+            Debug.Assert(reader != null, "reader != null");
 
-        public static Terraria.DataStructures.PlayerDeathReason ReadPlayerDeathReason(this BinaryReader reader) =>
-            Terraria.DataStructures.PlayerDeathReason.FromReader(reader);
+            return TerrariaNetworkText.Deserialize(reader);
+        }
 
-        public static Vector2 ReadVector2(this BinaryReader reader) =>
-            new Vector2(reader.ReadSingle(), reader.ReadSingle());
+        public static Terraria.DataStructures.PlayerDeathReason ReadPlayerDeathReason(this BinaryReader reader) {
+            Debug.Assert(reader != null, "reader != null");
+
+            return Terraria.DataStructures.PlayerDeathReason.FromReader(reader);
+        }
+
+        public static Vector2 ReadVector2(this BinaryReader reader) {
+            Debug.Assert(reader != null, "reader != null");
+
+            return new Vector2(reader.ReadSingle(), reader.ReadSingle());
+        }
 
         public static void Write(this BinaryWriter writer, Color color) {
+            Debug.Assert(writer != null, "writer != null");
+
             writer.Write(color.R);
             writer.Write(color.G);
             writer.Write(color.B);
@@ -42,14 +57,20 @@ namespace Orion.Packets.Extensions {
 
         public static void Write(this BinaryWriter writer,
                                  Terraria.DataStructures.PlayerDeathReason playerDeathReason) {
+            Debug.Assert(writer != null, "writer != null");
+
             playerDeathReason.WriteSelfTo(writer);
         }
 
         public static void Write(this BinaryWriter writer, TerrariaNetworkText text) {
+            Debug.Assert(writer != null, "writer != null");
+
             text.Serialize(writer);
         }
 
         public static void Write(this BinaryWriter writer, Vector2 vector) {
+            Debug.Assert(writer != null, "writer != null");
+
             writer.Write(vector.X);
             writer.Write(vector.Y);
         }
