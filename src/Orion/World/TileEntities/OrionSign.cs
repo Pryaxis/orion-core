68ec5c4a235d2f1f5263e56c0e66bf00f4613199
Kevin Zhao <kzhao1729@gmail.com>
2019-09-28 21:34:59 -0700

    Add OrionSign.

---
+++ src/Orion/World/TileEntities/OrionSign.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics;
+using Orion.Utils;
+
+namespace Orion.World.TileEntities {
+    internal sealed class OrionSign : AnnotatableObject, ISign {
+        public TileEntityType Type => TileEntityType.Sign;
+
+        public int Index { get; }
+
+        public int X {
+            get => Wrapped.x;
+            set => Wrapped.x = value;
+        }
+
+        public int Y {
+            get => Wrapped.y;
+            set => Wrapped.y = value;
+        }
+
+        public string Text {
+            get => Wrapped.text;
+            set => Wrapped.text = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        // internal for testing.
+        internal Terraria.Sign Wrapped { get; }
+
+        public OrionSign(int signIndex, Terraria.Sign terrariaSign) {
+            Debug.Assert(signIndex >= 0 && signIndex < Terraria.Main.maxChests,
+                         "chestIndex >= 0 && chestIndex < Terraria.Main.maxChests");
+            Debug.Assert(terrariaSign != null, "terrariaChest != null");
+
+            Index = signIndex;
+            Wrapped = terrariaSign;
+        }
+    }
+}
---
+++ tests/Orion.Tests/World/TileEntities/OrionSignTests.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2019 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Xunit;
+
+namespace Orion.World.TileEntities {
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class OrionSignTests {
+        [Fact]
+        public void GetX_IsCorrect() {
+            var terrariaSign = new Terraria.Sign {x = 100};
+            ISign sign = new OrionSign(0, terrariaSign);
+
+            sign.X.Should().Be(100);
+        }
+
+        [Fact]
+        public void SetX_IsCorrect() {
+            var terrariaSign = new Terraria.Sign();
+            ISign sign = new OrionSign(0, terrariaSign);
+
+            sign.X = 100;
+
+            terrariaSign.x.Should().Be(100);
+        }
+
+        [Fact]
+        public void GetY_IsCorrect() {
+            var terrariaSign = new Terraria.Sign {y = 100};
+            ISign sign = new OrionSign(0, terrariaSign);
+
+            sign.Y.Should().Be(100);
+        }
+
+        [Fact]
+        public void SetY_IsCorrect() {
+            var terrariaSign = new Terraria.Sign();
+            ISign sign = new OrionSign(0, terrariaSign);
+
+            sign.Y = 100;
+
+            terrariaSign.y.Should().Be(100);
+        }
+
+        [Fact]
+        public void GetText_IsCorrect() {
+            var terrariaSign = new Terraria.Sign {text = "test"};
+            ISign sign = new OrionSign(0, terrariaSign);
+
+            sign.Text.Should().Be("test");
+        }
+
+        [Fact]
+        public void SetText_IsCorrect() {
+            var terrariaSign = new Terraria.Sign();
+            ISign sign = new OrionSign(0, terrariaSign);
+
+            sign.Text = "test";
+
+            terrariaSign.text.Should().Be("test");
+        }
+
+        [Fact]
+        public void SetText_NullValue_ThrowsArgumentNullException() {
+            var terrariaSign = new Terraria.Sign();
+            ISign sign = new OrionSign(0, terrariaSign);
+            Action action = () => sign.Text = null!;
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
