1c9fb89157618356574ff621d5fa7241a5bb2842
Kevin Zhao <kzhao1729@gmail.com>
2020-05-23 00:00:58 -0700

    Implement Color3.

---
+++ src/Orion/Packets/DataStructures/Color3.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Orion.Packets.DataStructures {
+    /// <summary>
+    /// Represents a three byte component color.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Color3 : IEquatable<Color3> {
+        /// <summary>
+        /// The color white.
+        /// </summary>
+        public static readonly Color3 White = new Color3(255, 255, 255);
+
+        /// <summary>
+        /// Gets the red component.
+        /// </summary>
+        /// <value>The red component.</value>
+        [field: FieldOffset(0)]
+        public byte R { get; }
+
+        /// <summary>
+        /// Gets the green component.
+        /// </summary>
+        /// <value>The green component.</value>
+        [field: FieldOffset(1)]
+        public byte G { get; }
+
+        /// <summary>
+        /// Gets the blue component.
+        /// </summary>
+        /// <value>The blue component.</value>
+        [field: FieldOffset(2)]
+        public byte B { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Color3"/> structure with the specified components.
+        /// </summary>
+        /// <param name="r">The red component.</param>
+        /// <param name="g">The green component.</param>
+        /// <param name="b"></param>
+        public Color3(byte r, byte g, byte b) {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        /// <inheritdoc/>
+        [Pure]
+        public override bool Equals(object obj) => obj is Color3 color && Equals(color);
+
+        /// <inheritdoc/>
+        [Pure]
+        public bool Equals(Color3 other) => R == other.R && G == other.G && B == other.B;
+
+        /// <inheritdoc/>
+        [Pure]
+        public override int GetHashCode() => HashCode.Combine(R, G, B);
+
+        /// <summary>
+        /// Returns a string representation of the color.
+        /// </summary>
+        /// <returns>A string representation of the color.</returns>
+        [Pure, ExcludeFromCodeCoverage]
+        public override string ToString() => $"#{R:x2}{G:x2}{B:x2}";
+
+        /// <summary>
+        /// Determines whether two colors are equal.
+        /// </summary>
+        /// <param name="left">The left color.</param>
+        /// <param name="right">The right color.</param>
+        /// <returns><see langword="true"/> if the colors are equal; otherwise, <see langword="false"/>.</returns>
+        [Pure]
+        public static bool operator ==(Color3 left, Color3 right) => left.Equals(right);
+
+        /// <summary>
+        /// Determines whether two colors are not equal.
+        /// </summary>
+        /// <param name="left">The left color.</param>
+        /// <param name="right">The right color.</param>
+        /// <returns><see langword="true"/> if the colors are not equal; otherwise, <see langword="false"/>.</returns>
+        [Pure]
+        public static bool operator !=(Color3 left, Color3 right) => !left.Equals(right);
+    }
+}
---
+++ tests/Orion.Tests/Packets/DataStructures/Color3Tests.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Packets.DataStructures {
+    public class Color3Tests {
+        [Fact]
+        public void R_Get() {
+            var color = new Color3(1, 2, 3);
+
+            Assert.Equal(1, color.R);
+        }
+
+        [Fact]
+        public void G_Get() {
+            var color = new Color3(1, 2, 3);
+
+            Assert.Equal(2, color.G);
+        }
+
+        [Fact]
+        public void B_Get() {
+            var color = new Color3(1, 2, 3);
+
+            Assert.Equal(3, color.B);
+        }
+
+        [Fact]
+        public void Equals_ReturnsTrue() {
+            var color = new Color3(11, 22, 33);
+            var color2 = new Color3(11, 22, 33);
+
+            Assert.True(color.Equals((object)color2));
+            Assert.True(color.Equals(color2));
+        }
+
+        [Fact]
+        public void Equals_ReturnsFalse() {
+            var color = new Color3(255, 255, 255);
+            var color2 = new Color3(255, 255, 0);
+            var color3 = new Color3(255, 0, 0);
+            var color4 = new Color3(0, 0, 0);
+
+            Assert.False(color.Equals((object)color2));
+            Assert.False(color.Equals((object)color3));
+            Assert.False(color.Equals((object)color4));
+            Assert.False(color.Equals(color2));
+            Assert.False(color.Equals(color3));
+            Assert.False(color.Equals(color4));
+        }
+
+        [Fact]
+        public void GetHashCode_Equals_AreEqual() {
+            var color = new Color3(11, 22, 33);
+            var color2 = new Color3(11, 22, 33);
+
+            Assert.Equal(color.GetHashCode(), color2.GetHashCode());
+        }
+
+        [Fact]
+        public void op_Equality_ReturnsTrue() {
+            var color = new Color3(11, 22, 33);
+            var color2 = new Color3(11, 22, 33);
+
+            Assert.True(color == color2);
+        }
+
+        [Fact]
+        public void op_Equality_ReturnsFalse() {
+            var color = new Color3(255, 255, 255);
+            var color2 = new Color3(255, 255, 0);
+            var color3 = new Color3(255, 0, 0);
+            var color4 = new Color3(0, 0, 0);
+
+            Assert.False(color == color2);
+            Assert.False(color == color3);
+            Assert.False(color == color4);
+        }
+
+        [Fact]
+        public void op_Inequality_ReturnsTrue() {
+            var color = new Color3(255, 255, 255);
+            var color2 = new Color3(255, 255, 0);
+            var color3 = new Color3(255, 0, 0);
+            var color4 = new Color3(0, 0, 0);
+
+            Assert.True(color != color2);
+            Assert.True(color != color3);
+            Assert.True(color != color4);
+        }
+
+        [Fact]
+        public void op_Inequality_ReturnsFalse() {
+            var color = new Color3(11, 22, 33);
+            var color2 = new Color3(11, 22, 33);
+
+            Assert.False(color != color2);
+        }
+    }
+}
