54f889a36d936728f3065184af40144a2896cddf
Kevin Zhao <kzhao1729@gmail.com>
2020-05-24 19:22:30 -0700

    Introduce the IItem interface.

---
+++ src/Orion/Items/IItem.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using Orion.Entities;
+
+namespace Orion.Items {
+    /// <summary>
+    /// Represents a Terraria item.
+    /// </summary>
+    public interface IItem : IEntity, IWrapping<Terraria.Item> {
+        /// <summary>
+        /// Gets the item's ID.
+        /// </summary>
+        /// <value>The item's ID.</value>
+        ItemId Id { get; }
+
+        /// <summary>
+        /// Sets the item's <paramref name="id"/>. This will update the item accordingly. 
+        /// </summary>
+        /// <param name="id">The item ID.</param>
+        void SetId(ItemId id);
+    }
+}
---
+++ tests/Orion.Tests/Items/OrionItemTests.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Orion.Items {
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class OrionItemTests {
+        [Fact]
+        public void Name_Get() {
+            var terrariaItem = new Terraria.Item { _nameOverride = "test" };
+            var item = new OrionItem(terrariaItem);
+
+            Assert.Equal("test", item.Name);
+        }
+
+        [Fact]
+        public void Name_SetNullValue_ThrowsArgumentNullException() {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            Assert.Throws<ArgumentNullException>(() => item.Name = null!);
+        }
+
+        [Fact]
+        public void Name_Set() {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.Name = "test";
+
+            Assert.Equal("test", terrariaItem.Name);
+        }
+
+        [Fact]
+        public void Type_Get() {
+            var terrariaItem = new Terraria.Item { type = (int)ItemId.Sdmg };
+            var item = new OrionItem(terrariaItem);
+
+            Assert.Equal(ItemId.Sdmg, item.Id);
+        }
+
+        [Fact]
+        public void SetId() {
+            var terrariaItem = new Terraria.Item();
+            var item = new OrionItem(terrariaItem);
+
+            item.SetId(ItemId.Sdmg);
+
+            Assert.Equal(ItemId.Sdmg, (ItemId)terrariaItem.type);
+        }
+    }
+}
