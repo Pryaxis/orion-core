93f1740194456efefa4f1e18c97142a573f74cae
Kevin Zhao <kzhao1729@gmail.com>
2020-06-10 23:09:32 -0700

    Implement WireActivatePacket.

---
+++ src/Orion.Core/Packets/World/Tiles/WireActivatePacket.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Orion.Core.Packets.World.Tiles {
+    /// <summary>
+    /// Packet sent to activate a wire.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct WireActivatePacket : IPacket {
+        /// <summary>
+        /// Gets or sets the wire's X coordinate.
+        /// </summary>
+        /// <value>The wire's X coordinate.</value>
+        [field: FieldOffset(0)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wire's Y coordinate.
+        /// </summary>
+        /// <value>The wire's Y coordinate.</value>
+        [field: FieldOffset(2)] public short Y { get; set; }
+
+        PacketId IPacket.Id => PacketId.WireActivate;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) {
+            Unsafe.CopyBlockUnaligned(ref this.AsRefByte(0), ref span[0], 4);
+            return 4;
+        }
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) {
+            Unsafe.CopyBlockUnaligned(ref span[0], ref this.AsRefByte(0), 4);
+            return 4;
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/Tiles/WireActivatePacketTests.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World.Tiles {
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class WireActivatePacketTests {
+        public static readonly byte[] Bytes = { 7, 0, 59, 0, 1, 100, 0 };
+
+        [Fact]
+        public void X_Set_Get() {
+            var packet = new WireActivatePacket();
+
+            packet.X = 256;
+
+            Assert.Equal(256, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get() {
+            var packet = new WireActivatePacket();
+
+            packet.Y = 100;
+
+            Assert.Equal(100, packet.Y);
+        }
+
+        [Fact]
+        public void Read() {
+            var packet = new WireActivatePacket();
+            var span = Bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+
+            Assert.Equal(256, packet.X);
+            Assert.Equal(100, packet.Y);
+        }
+
+        [Fact]
+        public void RoundTrip() {
+            TestUtils.RoundTripPacket<WireActivatePacket>(Bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
