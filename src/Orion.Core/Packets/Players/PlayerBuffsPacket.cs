200c46328de8c288369a15e1630ee78ac9f50a3f
kevzhao2 <kzhao1729@gmail.com>
2020-06-21 14:17:40 -0700

    Implement PlayerBuffsPacket.

---
+++ src/Orion.Core/Packets/Players/PlayerBuffsPacket.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Orion.Core.Buffs;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to set a player's buffs.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct PlayerBuffsPacket : IPacket
+    {
+        [field: FieldOffset(1)] private unsafe fixed short _buffs[22];
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        /// <value>The player index.</value>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets the buff IDs.
+        /// </summary>
+        /// <value>The buff IDs.</value>
+        public unsafe Span<BuffId> Ids => MemoryMarshal.CreateSpan(ref Unsafe.As<short, BuffId>(ref _buffs[0]), 22);
+
+        PacketId IPacket.Id => PacketId.PlayerBuffs;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 45);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 45);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerBuffsPacketTests.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Orion.Core.Buffs;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class PlayerBuffsPacketTests
+    {
+        private readonly byte[] _bytes = {
+            48, 0, 50, 5, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+            0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0
+        };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerBuffsPacket();
+
+            packet.PlayerIndex = 5;
+
+            Assert.Equal(5, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void Ids_Set_Get()
+        {
+            var packet = new PlayerBuffsPacket();
+
+            for (var i = 0; i < packet.Ids.Length; ++i)
+            {
+                packet.Ids[i] = BuffId.ObsidianSkin;
+            }
+
+            for (var i = 0; i < packet.Ids.Length; ++i)
+            {
+                Assert.Equal(BuffId.ObsidianSkin, packet.Ids[i]);
+            }
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = new PlayerBuffsPacket();
+            var span = _bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+
+            Assert.Equal(5, packet.PlayerIndex);
+
+            for (var i = 0; i < packet.Ids.Length; ++i)
+            {
+                Assert.Equal(BuffId.ObsidianSkin, packet.Ids[i]);
+            }
+        }
+
+        [Fact]
+        public void RoundTrip()
+        {
+            TestUtils.RoundTripPacket<PlayerBuffsPacket>(_bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
