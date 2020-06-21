01d3caff70ff0624d15ad9f618b1a7eeef58ef4e
kevzhao2 <kzhao1729@gmail.com>
2020-06-21 15:39:32 -0700

    Implement ItemOwnerPacket.

---
+++ src/Orion.Core/Packets/Items/ItemOwnerPacket.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Orion.Core.Packets.Items
+{
+    /// <summary>
+    /// A packet sent to set an item's owner.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct ItemOwnerPacket : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the item's index.
+        /// </summary>
+        /// <value>The item's index.</value>
+        [field: FieldOffset(0)] public short ItemIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item's owner index. If <c>255</c>, then there is no owner.
+        /// </summary>
+        /// <value>The item's owner index.</value>
+        [field: FieldOffset(2)] public byte OwnerIndex { get; set; }
+
+        PacketId IPacket.Id => PacketId.ItemOwner;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 3);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 3);
+    }
+}
