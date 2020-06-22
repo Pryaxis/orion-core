c5e382cc69ecbbbb8b64165c53a7d876e148a54b
Kevin Zhao <kzhao1729@gmail.com>
2020-06-21 22:11:15 -0700

    Implement NpcDamagePacket.

---
+++ src/Orion.Core/Packets/Npcs/NpcDamagePacket.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent to damage an NPC.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct NpcDamagePacket : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the NPC index.
+        /// </summary>
+        /// <value>The NPC index.</value>
+        [field: FieldOffset(0)] public short NpcIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the damage. If <c>-1</c> and read in <see cref="PacketContext.Client"/>, then the NPC is
+        /// removed.
+        /// </summary>
+        /// <value>The damage.</value>
+        [field: FieldOffset(2)] public short Damage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the knockback.
+        /// </summary>
+        /// <value>The knockback.</value>
+        [field: FieldOffset(4)] public float Knockback { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hit direction.
+        /// </summary>
+        /// <value>The hit direction.</value>
+        [field: FieldOffset(8)] public HitDirection Direction { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this is a critical hit.
+        /// </summary>
+        /// <value><see langword="true"/> if this is a critical hit; otherwise, <see langword="false"/>.</value>
+        [field: FieldOffset(9)] public bool IsCritical { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcDamage;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 10);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 10);
+
+        /// <summary>
+        /// Specifies the hit direction in a <see cref="NpcDamagePacket"/>.
+        /// </summary>
+        public enum HitDirection : byte
+        {
+            /// <summary>
+            /// Indicates left.
+            /// </summary>
+            Left = 0,
+
+            /// <summary>
+            /// Indicates right.
+            /// </summary>
+            Right = 2
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcDamagePacketTests.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class NpcDamagePacketTests
+    {
+        private readonly byte[] _bytes = { 13, 0, 28, 5, 0, 108, 0, 205, 204, 128, 64, 2, 1 };
+
+        [Fact]
+        public void NpcIndex_Set_Get()
+        {
+            var packet = new NpcDamagePacket();
+
+            packet.NpcIndex = 5;
+
+            Assert.Equal(5, packet.NpcIndex);
+        }
+
+        [Fact]
+        public void Damage_Set_Get()
+        {
+            var packet = new NpcDamagePacket();
+
+            packet.Damage = 108;
+
+            Assert.Equal(108, packet.Damage);
+        }
+
+        [Fact]
+        public void Knockback_Set_Get()
+        {
+            var packet = new NpcDamagePacket();
+
+            packet.Knockback = 4.025f;
+
+            Assert.Equal(4.025f, packet.Knockback);
+        }
+
+        [Fact]
+        public void Direction_Set_Get()
+        {
+            var packet = new NpcDamagePacket();
+
+            packet.Direction = NpcDamagePacket.HitDirection.Right;
+
+            Assert.Equal(NpcDamagePacket.HitDirection.Right, packet.Direction);
+        }
+
+        [Fact]
+        public void IsCritical_Set_Get()
+        {
+            var packet = new NpcDamagePacket();
+
+            packet.IsCritical = true;
+
+            Assert.True(packet.IsCritical);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = new NpcDamagePacket();
+            var span = _bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+
+            Assert.Equal(5, packet.NpcIndex);
+            Assert.Equal(108, packet.Damage);
+            Assert.Equal(4.025f, packet.Knockback);
+            Assert.Equal(NpcDamagePacket.HitDirection.Right, packet.Direction);
+            Assert.True(packet.IsCritical);
+        }
+
+        [Fact]
+        public void RoundTrip()
+        {
+            TestUtils.RoundTripPacket<NpcDamagePacket>(_bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
