e3d77ff5d9caac00da98dda6944b488b9bae7de2
Kevin Zhao <kzhao1729@gmail.com>
2020-06-22 00:00:13 -0700

    Implement PlayerHealPacket.

---
+++ src/Orion.Core/Packets/Players/PlayerHealPacket.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to heal a player.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct PlayerHealPacket : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        /// <value>The player index.</value>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the heal amount.
+        /// </summary>
+        /// <value>The heal amount.</value>
+        [field: FieldOffset(1)] public short Amount { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerHeal;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 3);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 3);
+    }
+}
