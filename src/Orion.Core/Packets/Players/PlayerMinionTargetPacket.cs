79ea55e08c7d76a15b011215f97cc18ab7645029
Kevin Zhao <kzhao1729@gmail.com>
2020-06-23 01:30:54 -0700

    Implement PlayerMinionTargetPacket.

---
+++ src/Orion.Core/Packets/Players/PlayerMinionTargetPacket.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to set a player's minion target.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct PlayerMinionTargetPacket : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        /// <value>The player index.</value>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player's minion target NPC index.
+        /// </summary>
+        /// <value>The player's minion target NPC index.</value>
+        [field: FieldOffset(1)] public short NpcIndex { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerMinionTarget;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 3);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 3);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerMinionTargetPacketTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class PlayerMinionTargetPacketTests
+    {
+        private readonly byte[] _bytes = { 6, 0, 115, 5, 1, 0 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerMinionTargetPacket();
+
+            packet.PlayerIndex = 5;
+
+            Assert.Equal(5, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void NpcIndex_Set_Get()
+        {
+            var packet = new PlayerMinionTargetPacket();
+
+            packet.NpcIndex = 1;
+
+            Assert.Equal(1, packet.NpcIndex);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = new PlayerMinionTargetPacket();
+            var span = _bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+
+            Assert.Equal(5, packet.PlayerIndex);
+            Assert.Equal(1, packet.NpcIndex);
+        }
+
+        [Fact]
+        public void RoundTrip()
+        {
+            TestUtils.RoundTripPacket<PlayerMinionTargetPacket>(
+                _bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
