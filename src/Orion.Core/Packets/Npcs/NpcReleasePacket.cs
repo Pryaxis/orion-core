135d7d4b38cf319680cfc4df1e270fa26d76197d
kevzhao2 <kzhao1729@gmail.com>
2020-06-22 17:47:41 -0700

    Implement NpcReleasePacket.

---
+++ src/Orion.Core/Packets/Npcs/NpcReleasePacket.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+using Orion.Core.Npcs;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent from the client to the server to release an NPC.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct NpcReleasePacket : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the NPC's X position.
+        /// </summary>
+        /// <value>The NPC's X position.</value>
+        [field: FieldOffset(0)] public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NPC's Y position.
+        /// </summary>
+        /// <value>The NPC's Y position.</value>
+        [field: FieldOffset(4)] public int Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NPC ID.
+        /// </summary>
+        /// <value>The NPC ID.</value>
+        [field: FieldOffset(8)] public NpcId Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NPC style.
+        /// </summary>
+        /// <value>The NPC style.</value>
+        [field: FieldOffset(10)] public byte Style { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcRelease;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 11);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 11);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcReleasePacketTests.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Orion.Core.Npcs;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class NpcReleasePacketTests
+    {
+        private readonly byte[] _bytes = { 14, 0, 71, 64, 6, 0, 0, 0, 16, 0, 0, 100, 1, 1 };
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new NpcReleasePacket();
+
+            packet.X = 1600;
+
+            Assert.Equal(1600, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new NpcReleasePacket();
+
+            packet.Y = 4096;
+
+            Assert.Equal(4096, packet.Y);
+        }
+
+        [Fact]
+        public void Id_Set_Get()
+        {
+            var packet = new NpcReleasePacket();
+
+            packet.Id = NpcId.Butterfly;
+
+            Assert.Equal(NpcId.Butterfly, packet.Id);
+        }
+
+        [Fact]
+        public void Style_Set_Get()
+        {
+            var packet = new NpcReleasePacket();
+
+            packet.Style = 1;
+
+            Assert.Equal(1, packet.Style);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = new NpcReleasePacket();
+            var span = _bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+
+            Assert.Equal(1600, packet.X);
+            Assert.Equal(4096, packet.Y);
+            Assert.Equal(NpcId.Butterfly, packet.Id);
+            Assert.Equal(1, packet.Style);
+        }
+
+        [Fact]
+        public void RoundTrip()
+        {
+            TestUtils.RoundTripPacket<NpcReleasePacket>(_bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
