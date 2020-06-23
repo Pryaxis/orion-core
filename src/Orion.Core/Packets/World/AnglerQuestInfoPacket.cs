bf8a7c48ec01b1376dd126920427557c4bbee7d9
kevzhao2 <kzhao1729@gmail.com>
2020-06-22 18:00:42 -0700

    Implement AnglerQuestInfoPacket.

---
+++ src/Orion.Core/Packets/World/AnglerQuestInfoPacket.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent from the server to the client to set the angler quest.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct AnglerQuestInfoPacket : IPacket
+    {
+        // TODO: potentially enum-ify this.
+
+        /// <summary>
+        /// Gets or sets the angler quest.
+        /// </summary>
+        /// <value>The angler quest.</value>
+        [field: FieldOffset(0)] public byte Quest { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the angler quest is completed.
+        /// </summary>
+        /// <value><see langword="true"/> if the angler quest is completed; otherwise <see langword="false"/>.</value>
+        [field: FieldOffset(1)] public bool IsCompleted { get; set; }
+
+        PacketId IPacket.Id => PacketId.AnglerQuestInfo;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 2);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 2);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/AnglerQuestInfoPacketTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class AnglerQuestInfoPacketTests
+    {
+        private readonly byte[] _bytes = { 3, 0, 74, 10, 1 };
+
+        [Fact]
+        public void Quest_Set_Get()
+        {
+            var packet = new AnglerQuestInfoPacket();
+
+            packet.Quest = 10;
+
+            Assert.Equal(10, packet.Quest);
+        }
+
+        [Fact]
+        public void IsCompleted_Set_Get()
+        {
+            var packet = new AnglerQuestInfoPacket();
+
+            packet.IsCompleted = true;
+
+            Assert.True(packet.IsCompleted);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = new AnglerQuestInfoPacket();
+            var span = _bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+        }
+
+        [Fact]
+        public void RoundTrip()
+        {
+            TestUtils.RoundTripPacket<AnglerQuestInfoPacket>(
+                _bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
