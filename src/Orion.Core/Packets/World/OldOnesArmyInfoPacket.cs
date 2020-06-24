e7e36ef839ea3bc74919b2ca65d1472eac6f520f
Kevin Zhao <kzhao1729@gmail.com>
2020-06-23 19:15:21 -0700

    Implement OldOnesArmyInfoPacket.

---
+++ src/Orion.Core/Packets/World/OldOnesArmyInfoPacket.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent from the server to the client to set the Old One's Army event information.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct OldOnesArmyInfoPacket : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the number of ticks before the next wave.
+        /// </summary>
+        /// <value>The number of ticks before the next wave.</value>
+        [field: FieldOffset(0)] public int TicksBeforeNextWave { get; set; }
+
+        PacketId IPacket.Id => PacketId.OldOnesArmyInfo;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 4);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 4);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/OldOnesArmyInfoPacketTests.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class OldOnesArmyInfoPacketTests
+    {
+        private readonly byte[] _bytes = { 7, 0, 116, 16, 14, 0, 0 };
+
+        [Fact]
+        public void TicksBeforeNextWave_Set_Get()
+        {
+            var packet = new OldOnesArmyInfoPacket();
+
+            packet.TicksBeforeNextWave = 3600;
+
+            Assert.Equal(3600, packet.TicksBeforeNextWave);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = new OldOnesArmyInfoPacket();
+            var span = _bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+
+            Assert.Equal(3600, packet.TicksBeforeNextWave);
+        }
+
+        [Fact]
+        public void RoundTrip()
+        {
+            TestUtils.RoundTripPacket<OldOnesArmyInfoPacket>(_bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
