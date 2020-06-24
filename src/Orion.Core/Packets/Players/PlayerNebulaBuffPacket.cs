26e07250019037b6bcc19ad9f402ff6b892f2b90
Kevin Zhao <kzhao1729@gmail.com>
2020-06-23 19:25:54 -0700

    Implement PlayerNebulaBuffPacket.

---
+++ src/Orion.Core/Packets/Players/PlayerNebulaBuffPacket.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Buffs;
+using Orion.Core.DataStructures;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to buff a player with a Nebula Armor buff.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct PlayerNebulaBuffPacket : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        /// <value>The player index.</value>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the buff ID.
+        /// </summary>
+        /// <value>The buff ID.</value>
+        [field: FieldOffset(1)] public BuffId Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the buff's position.
+        /// </summary>
+        /// <value>The buff's position.</value>
+        [field: FieldOffset(3)] public Vector2f Position { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerNebulaBuff;
+
+        /// <inheritdoc/>
+        public int Read(Span<byte> span, PacketContext context) => span.Read(ref this.AsRefByte(0), 11);
+
+        /// <inheritdoc/>
+        public int Write(Span<byte> span, PacketContext context) => span.Write(ref this.AsRefByte(0), 11);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerNebulaBuffPacketTests.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Orion.Core.Buffs;
+using Orion.Core.DataStructures;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class PlayerNebulaBuffPacketTests
+    {
+        private readonly byte[] _bytes = { 14, 0, 102, 5, 173, 0, 0, 0, 200, 66, 0, 0, 128, 67 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerNebulaBuffPacket();
+
+            packet.PlayerIndex = 5;
+
+            Assert.Equal(5, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void Id_Set_Get()
+        {
+            var packet = new PlayerNebulaBuffPacket();
+
+            packet.Id = BuffId.LifeNebula;
+
+            Assert.Equal(BuffId.LifeNebula, packet.Id);
+        }
+
+        [Fact]
+        public void Position_Set_Get()
+        {
+            var packet = new PlayerNebulaBuffPacket();
+
+            packet.Position = new Vector2f(100, 256);
+
+            Assert.Equal(new Vector2f(100, 256), packet.Position);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = new PlayerNebulaBuffPacket();
+            var span = _bytes.AsSpan(IPacket.HeaderSize..);
+            Assert.Equal(span.Length, packet.Read(span, PacketContext.Server));
+
+            Assert.Equal(5, packet.PlayerIndex);
+            Assert.Equal(BuffId.LifeNebula, packet.Id);
+            Assert.Equal(new Vector2f(100, 256), packet.Position);
+        }
+
+        [Fact]
+        public void RoundTrip()
+        {
+            TestUtils.RoundTripPacket<PlayerNebulaBuffPacket>(
+                _bytes.AsSpan(IPacket.HeaderSize..), PacketContext.Server);
+        }
+    }
+}
