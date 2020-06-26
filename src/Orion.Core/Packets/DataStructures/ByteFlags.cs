a3f579e25737983c763aae054c303bf37f309595
Kevin Zhao <kzhao1729@gmail.com>
2020-06-26 02:08:28 -0700

    Implement ByteFlags.

---
+++ src/Orion.Core/Packets/DataStructures/ByteFlags.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Orion.Core.Packets.DataStructures
+{
+    /// <summary>
+    /// Provides bit-based indexing into a byte as flags.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct ByteFlags
+    {
+        [FieldOffset(0)] private byte _value;
+
+        /// <summary>
+        /// Gets or sets the boolean flag at the specified <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns>The boolean flag at the specified <paramref name="index"/>.</returns>
+        public bool this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                Debug.Assert(0 <= index && index < 8);
+
+                return (_value & (1 << index)) != 0;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                Debug.Assert(0 <= index && index < 8);
+
+                if (value)
+                {
+                    _value |= (byte)(1 << index);
+                }
+                else
+                {
+                    _value &= (byte)~(1 << index);
+                }
+            }
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/DataStructures/ByteFlagsTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.DataStructures
+{
+    public class ByteFlagsTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        public void Item_Set_Get(int index)
+        {
+            var flags = new ByteFlags();
+
+            flags[index] = true;
+
+            Assert.True(flags[index]);
+
+            flags[index] = false;
+
+            Assert.False(flags[index]);
+        }
+    }
+}
