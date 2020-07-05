c3cca9ff6ced4666f816ed8864269a88cddfb646
Kevin Zhao <kzhao1729@gmail.com>
2020-07-05 01:39:20 -0700

    Implement NpcKilled.

---
+++ src/Orion.Core/Packets/Npcs/NpcKilled.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Npcs;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent from the server to the client to indicate that an NPC ID has been killed.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 2)]
+    public struct NpcKilled : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes;  // Used to obtain an interior reference.
+
+        /// <summary>
+        /// Gets or sets the NPC ID.
+        /// </summary>
+        /// <value>The NPC ID.</value>
+        [field: FieldOffset(0)] public NpcId Id { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcKilled;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 2);
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 2);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcKilledTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Diagnostics.CodeAnalysis;
+using Orion.Core.Npcs;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class NpcKilledTests
+    {
+        private readonly byte[] _bytes = { 5, 0, 97, 1, 0 };
+
+        [Fact]
+        public void Id_Set_Get()
+        {
+            var packet = new NpcKilled();
+
+            packet.Id = NpcId.BlueSlime;
+
+            Assert.Equal(NpcId.BlueSlime, packet.Id);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<NpcKilled>(_bytes, PacketContext.Server);
+
+            Assert.Equal(NpcId.BlueSlime, packet.Id);
+        }
+    }
+}
