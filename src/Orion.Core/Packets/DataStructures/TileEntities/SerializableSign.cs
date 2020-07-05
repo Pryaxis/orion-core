e8a29d0fa79659b74980fb0c36ff2633cd777539
Kevin Zhao <kzhao1729@gmail.com>
2020-07-05 02:14:19 -0700

    Implement SerializableSign.
    This is required for the upcoming SectionInfo packet.

---
+++ src/Orion.Core/Packets/DataStructures/TileEntities/SerializableSign.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.DataStructures.TileEntities
+{
+    /// <summary>
+    /// Represents a serializable sign.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct SerializableSign
+    {
+        [FieldOffset(0)] private byte _bytes;  // Used to obtain an interior reference.
+        [FieldOffset(8)] private string? _text;
+
+        /// <summary>
+        /// Gets or sets the sign's index.
+        /// </summary>
+        /// <value>The sign's index.</value>
+        [field: FieldOffset(0)] public short Index { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sign's X coordinate.
+        /// </summary>
+        /// <value>The sign's X coordinate.</value>
+        [field: FieldOffset(2)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sign's Y coordinate.
+        /// </summary>
+        /// <value>The sign's Y coordinate.</value>
+        [field: FieldOffset(4)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sign's text.
+        /// </summary>
+        /// <value>The sign's text.</value>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+        public string Text
+        {
+            get => _text ??= string.Empty;
+            set => _text = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <summary>
+        /// Writes the sign to the given <paramref name="span"/>. Returns the number of bytes written to the
+        /// <paramref name="span"/>.
+        /// </summary>
+        /// <param name="span">The span to write to.</param>
+        /// <returns>The number of bytes written to the <paramref name="span"/>.</returns>
+        public int Write(Span<byte> span)
+        {
+            var length = span.Write(ref _bytes, 6);
+            length += span[length..].Write(Text);
+            return length;
+        }
+
+        /// <summary>
+        /// Reads a <see cref="SerializableSign"/> instance from the given <paramref name="span"/>. Returns the number
+        /// of bytes read from the <paramref name="span"/>.
+        /// </summary>
+        /// <param name="span">The span to read from.</param>
+        /// <param name="sign">The resulting <see cref="SerializableSign"/> instance.</param>
+        /// <returns>The number of bytes read from the <paramref name="span"/>.</returns>
+        public static int Read(Span<byte> span, out SerializableSign sign)
+        {
+            sign = default;
+
+            var length = span.Read(ref sign._bytes, 6);
+            length += span[length..].Read(out sign._text);
+            return length;
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/DataStructures/TileEntities/SerializableSignTests.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Orion.Core.Packets.DataStructures.TileEntities
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class SerializableSignTests
+    {
+        private readonly byte[] _bytes = { 5, 0, 0, 1, 100, 0, 4, 116, 101, 115, 116 };
+
+        [Fact]
+        public void Index_Set_Get()
+        {
+            var sign = new SerializableSign();
+
+            sign.Index = 5;
+
+            Assert.Equal(5, sign.Index);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var sign = new SerializableSign();
+
+            sign.X = 256;
+
+            Assert.Equal(256, sign.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var sign = new SerializableSign();
+
+            sign.Y = 100;
+
+            Assert.Equal(100, sign.Y);
+        }
+
+        [Fact]
+        public void Text_Set_Get()
+        {
+            var sign = new SerializableSign();
+
+            sign.Text = "test";
+
+            Assert.Equal("test", sign.Text);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var length = SerializableSign.Read(_bytes, out var sign);
+            Assert.Equal(_bytes.Length, length);
+
+            Assert.Equal(5, sign.Index);
+            Assert.Equal(256, sign.X);
+            Assert.Equal(100, sign.Y);
+            Assert.Equal("test", sign.Text);
+
+            var bytes2 = new byte[1000];
+            var length2 = sign.Write(bytes2);
+
+            Assert.Equal(length, length2);
+            Assert.Equal(_bytes, bytes2[..length2]);
+        }
+    }
+}
