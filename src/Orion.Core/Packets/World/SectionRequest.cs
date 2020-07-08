50ef8f7e1ea167f11117d65cd18eb8c2f2e96cd5
Kevin Zhao <kzhao1729@gmail.com>
2020-07-08 01:23:02 -0700

    Implement SectionRequest.

---
+++ src/Orion.Core/Packets/World/SectionRequest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent from the client to the server to request a section before joining.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 8)]
+    public struct SectionRequest : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes;  // Used to obtain an interior reference.
+
+        /// <summary>
+        /// Gets or sets the section's X coordinate. A value of <c>-1</c> indicates the spawn section.
+        /// </summary>
+        /// <value>The section's X coordinate.</value>
+        [field: FieldOffset(0)] public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the section's Y coordinate. A value of <c>-1</c> indicates the spawn section.
+        /// </summary>
+        /// <value>The section's Y coordinate.</value>
+        [field: FieldOffset(4)] public int Y { get; set; }
+
+        PacketId IPacket.Id => PacketId.SectionRequest;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 8);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 8);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/SectionRequestTests.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public class SectionRequestTests
+    {
+        private readonly byte[] _bytes = { 11, 0, 8, 0, 1, 0, 0, 100, 0, 0, 0 };
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new SectionRequest();
+
+            packet.X = 256;
+
+            Assert.Equal(256, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new SectionRequest();
+
+            packet.Y = 100;
+
+            Assert.Equal(100, packet.Y);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<SectionRequest>(_bytes, PacketContext.Server);
+
+            Assert.Equal(256, packet.X);
+            Assert.Equal(100, packet.Y);
+        }
+    }
+}
