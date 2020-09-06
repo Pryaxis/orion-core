d402ad256467af5bdc736068c1d750155ab01660
ivanbiljan <ssbiljan@gmail.com>
2020-09-06 13:26:37 +0200

    Implement WorldTime

---
+++ src/Orion.Core/Packets/World/WorldTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent to notify clients of the current world time.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 9)]
+    public struct WorldTime : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets a value indicating whether it is currently day time.
+        /// </summary>
+        [field: FieldOffset(0)] public bool IsDayTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time. One hour in-game lasts one minute real-world.
+        /// </summary>
+        [field: FieldOffset(1)] public int Time { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value that moves the sun texture across the Y axis.
+        /// </summary>
+        [field: FieldOffset(5)] public short SunOffsetY { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value that moves the moon texture across the Y axis.
+        /// </summary>
+        [field: FieldOffset(7)] public short MoonOffsetY { get; set; }
+
+        PacketId IPacket.Id => PacketId.WorldTime;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 9);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 9);
+    }
+}
---
+++ tests/Orion.Core.Tests/World/WorldTimeTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Orion.Core.Packets;
+using Orion.Core.Packets.World;
+using Xunit;
+
+namespace Orion.Core.World
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public sealed class WorldTimeTests
+    {
+        private readonly byte[] _bytes = { 11, 0, 18, 1, 180, 0, 0, 0, 25, 0, 0, 0 };
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void IsDayTime_Set_Get(bool value)
+        {
+            var packet = new WorldTime();
+
+            packet.IsDayTime = value;
+
+            Assert.Equal(value, packet.IsDayTime);
+        }
+
+        [Fact]
+        public void Time_Set_Get()
+        {
+            var packet = new WorldTime();
+
+            packet.Time = 4860;
+
+            Assert.Equal(4860, packet.Time);
+        }
+
+        [Fact]
+        public void SunOffsetY_Set_Get()
+        {
+            var packet = new WorldTime();
+
+            packet.SunOffsetY = 25;
+            
+            Assert.Equal(25, packet.SunOffsetY);
+        }
+
+        [Fact]
+        public void MoonOffsetY_Set_Get()
+        {
+            var packet = new WorldTime();
+
+            packet.MoonOffsetY = 25;
+
+            Assert.Equal(25, packet.MoonOffsetY);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<WorldTime>(_bytes, PacketContext.Client);
+
+            Assert.True(packet.IsDayTime);
+            Assert.Equal(180, packet.Time);
+            Assert.Equal(25, packet.SunOffsetY);
+            Assert.Equal(0, packet.MoonOffsetY);
+        }
+    }
+}
