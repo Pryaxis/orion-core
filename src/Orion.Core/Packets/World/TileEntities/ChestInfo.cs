e2ab8b8f412b1dc1fc5b569031885d7e511063b2
ivanbiljan <ssbiljan@gmail.com>
2020-09-06 20:40:15 +0200

    Implement ChestInfo

---
+++ src/Orion.Core/Packets/World/TileEntities/ChestInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World.TileEntities
+{
+    /// <summary>
+    /// A packet sent to unblock occupied chests.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 27, CharSet = CharSet.Auto)]
+    public struct ChestInfo : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+        [FieldOffset(6)] private readonly byte _nameLength;
+        [FieldOffset(8)] private string? _name;
+
+        /// <summary>
+        /// Gets or sets the chest index.
+        /// </summary>
+        [field: FieldOffset(0)] public short ChestIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest's X coordinate.
+        /// </summary>
+        [field: FieldOffset(2)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest's Y coordinate.
+        /// </summary>
+        [field: FieldOffset(4)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest's name.
+        /// </summary>
+        public string Name
+        {
+            get => _name ??= string.Empty;
+            set => _name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        PacketId IPacket.Id => PacketId.ChestInfo;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Read(ref _bytes, 6);
+            if (span[length] > 0 && span[length] <= 20)
+            {
+                length += span[length..].Read(out _name);
+            }
+
+            return length;
+        }
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Write(ref _bytes, 6);
+            if (Name.Length > 0 && Name.Length <= 20)
+            {
+                length += span[length..].Write(Name);
+            }
+
+            return length;
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/TileEntities/ChestInfoTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World.TileEntities
+{
+    public sealed class ChestInfoTests
+    {
+        private readonly byte[] _bytes = { 20, 0, 33, 5, 0, 150, 0, 200, 0, 10, 84, 101, 115, 116, 32, 67, 104, 101, 115, 116 };
+
+        [Fact]
+        public void ChestIndex_Set_Get()
+        {
+            var packet = new ChestInfo();
+
+            packet.ChestIndex = 10;
+
+            Assert.Equal(10, packet.ChestIndex);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new ChestInfo();
+
+            packet.X = 25;
+
+            Assert.Equal(25, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new ChestInfo();
+
+            packet.Y = 25;
+
+            Assert.Equal(25, packet.Y);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<ChestInfo>(_bytes, PacketContext.Server);
+            Assert.Equal(5, packet.ChestIndex);
+            Assert.Equal(150, packet.X);
+            Assert.Equal(200, packet.Y);
+            Assert.Equal("Test Chest", packet.Name);
+        }
+    }
+}
