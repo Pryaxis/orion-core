17978410c2efe07e4f8f8275b29f037fe0f7cab7
ivanbiljan <ssbiljan@gmail.com>
2020-09-07 01:57:26 +0200

    Implement WorldBiomes

---
+++ src/Orion.Core/Packets/World/WorldBiomes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent to update world biomes.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 3)]
+    public struct WorldBiomes : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the percentage of 'Good'.
+        /// </summary>
+        [field: FieldOffset(0)] public byte Good { get; set; }
+
+        /// <summary>
+        /// Gets or sets the percentage of 'Evil'.
+        /// </summary>
+        [field: FieldOffset(1)] public byte Evil { get; set; }
+
+        /// <summary>
+        /// Gets or sets the percentage of 'Crimson'.
+        /// </summary>
+        [field: FieldOffset(2)] public byte Crimson { get; set; }
+
+        PacketId IPacket.Id => PacketId.WorldBiomes;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 3);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 3);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/WorldBiomesTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    public sealed class WorldBiomesTests
+    {
+        private readonly byte[] _bytes = { 6, 0, 57, 10, 20, 30 };
+
+        [Fact]
+        public void Good_Set_Get()
+        {
+            var packet = new WorldBiomes();
+
+            packet.Good = 50;
+
+            Assert.Equal(50, packet.Good);
+        }
+
+        [Fact]
+        public void Evil_Set_Get()
+        {
+            var packet = new WorldBiomes();
+
+            packet.Evil = 50;
+
+            Assert.Equal(50, packet.Evil);
+        }
+
+        [Fact]
+        public void Crimson_Set_Get()
+        {
+            var packet = new WorldBiomes();
+
+            packet.Crimson = 50;
+
+            Assert.Equal(50, packet.Crimson);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<WorldBiomes>(_bytes, PacketContext.Server);
+
+            Assert.Equal(10, packet.Good);
+            Assert.Equal(20, packet.Evil);
+            Assert.Equal(30, packet.Crimson);
+        }
+    }
+}
