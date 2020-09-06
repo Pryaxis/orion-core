d62491f46cca892a0b1767557e2e0c3af4b1c360
ivanbiljan <ssbiljan@gmail.com>
2020-09-07 00:04:27 +0200

    Define SignInfo

---
+++ src/Orion.Core/Packets/World/TileEntities/SignInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Packets.DataStructures;
+using Orion.Core.Utils;
+using Serilog.Data;
+
+namespace Orion.Core.Packets.World.TileEntities
+{
+    /// <summary>
+    /// A packet sent to display and edit signs.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 10)]
+    public struct SignInfo : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+        [FieldOffset(8)] private string? _text;
+        [FieldOffset(16)] private byte _bytes2;
+        [FieldOffset(17)] private Flags8 _flags;
+
+        /// <summary>
+        /// Gets or sets the sign index.
+        /// </summary>
+        [field: FieldOffset(0)] public byte SignIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sign's X coordinate.
+        /// </summary>
+        [field: FieldOffset(1)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sign's Y coordinate.
+        /// </summary>
+        [field: FieldOffset(3)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        [field: FieldOffset(16)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sign text.
+        /// </summary>
+        public string Text
+        {
+            get => _text ??= string.Empty;
+            set => _text = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        PacketId IPacket.Id => PacketId.SignInfo;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Read(ref _bytes, 5);
+            length += span[length..].Read(out _text);
+            length += span[length..].Read(ref _bytes2, 2);
+            return length;
+        }
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Write(ref _bytes, 5);
+            length += span[length..].Write(Text);
+            length += span[length..].Write(ref _bytes2, 2);
+            return length;
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/TileEntities/SignInfoTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World.TileEntities
+{
+    public sealed class SignInfoTests
+    {
+        private readonly byte[] _bytes = { 21, 0, 47, 5, 100, 0, 100, 0, 11, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 250, 0 };
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<SignInfo>(_bytes, PacketContext.Server);
+
+            Assert.Equal(5, packet.SignIndex);
+            Assert.Equal(100, packet.X);
+            Assert.Equal(100, packet.Y);
+            Assert.Equal("ABCDEFGHIJK", packet.Text);
+            Assert.Equal(250, packet.PlayerIndex);
+        }
+    }
+}
