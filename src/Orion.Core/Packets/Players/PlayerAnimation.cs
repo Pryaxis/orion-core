5a7d5775147be9416fedc2b44c7e4c5bad189c00
ivanbiljan <ssbiljan@gmail.com>
2020-09-06 22:56:20 +0200

    Implement PlayerAnimation

---
+++ src/Orion.Core/Packets/Players/PlayerAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to set an item animation.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 7)]
+    public struct PlayerAnimation : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes;
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item rotation.
+        /// </summary>
+        [field: FieldOffset(1)] public float ItemRotation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the animation time (in frames).
+        /// </summary>
+        [field: FieldOffset(5)] public short AnimationTime { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerAnimation;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 7);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 7);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerAnimationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    public sealed class PlayerAnimationTests
+    {
+        private readonly byte[] _bytes = { 10, 0, 41, 200, 0, 0, 0, 63, 50, 0 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerAnimation();
+
+            packet.PlayerIndex = 5;
+
+            Assert.Equal(5, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void ItemRotation_Set_Get()
+        {
+            var packet = new PlayerAnimation();
+
+            packet.ItemRotation = 0.5F;
+
+            Assert.Equal(0.5F, packet.ItemRotation);
+        }
+
+        [Fact]
+        public void AnimationTime_Set_Get()
+        {
+            var packet = new PlayerAnimation();
+
+            packet.AnimationTime = 10;
+
+            Assert.Equal(10, packet.AnimationTime);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<PlayerAnimation>(_bytes, PacketContext.Server);
+
+            Assert.Equal(200, packet.PlayerIndex);
+            Assert.Equal(0.5F, packet.ItemRotation);
+            Assert.Equal(50, packet.AnimationTime);
+        }
+    }
+}
