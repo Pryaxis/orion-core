9a1449eeefcbfc174d821adc63d2ddcd06a75737
ivanbiljan <ssbiljan@gmail.com>
2020-09-06 22:14:25 +0200

    Implement ChestModify

---
+++ src/Orion.Core/Packets/World/TileEntities/ChestModify.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World.TileEntities
+{
+    /// <summary>
+    /// A packet sent to issue chest modifications, i.e placing and destroying chests.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 9)]
+    public struct ChestModify : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the type of modification.
+        /// </summary>
+        [field: FieldOffset(0)] public ChestModification Modification { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest's X coordinate.
+        /// </summary>
+        [field: FieldOffset(1)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest's Y coordinate.
+        /// </summary>
+        [field: FieldOffset(3)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest style.
+        /// </summary>
+        [field: FieldOffset(5)] public short Style { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest index. This specifies the chest to destroy when the corresponding modification is supplied.
+        /// </summary>
+        [field: FieldOffset(7)] public short ChestIndex { get; set; }
+
+        PacketId IPacket.Id => PacketId.ChestModify;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 9);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Write(ref _bytes, 9);
+            if (context == PacketContext.Client)
+            {
+                // Only the server sends the chest index
+                span[(length - 2)..].Fill(0);
+            }
+
+            return length;
+        }
+    }
+
+    /// <summary>
+    /// Specifies a <see cref="ChestModify"/> action.
+    /// </summary>
+    public enum ChestModification : byte
+    {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        PlaceChest,
+        DestroyChest,
+        PlaceDresser,
+        DestroyDresser,
+        PlaceContainer,
+        DestroyContainer
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/TileEntities/ChestModifyTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReLogic.Content;
+using Xunit;
+
+namespace Orion.Core.Packets.World.TileEntities
+{
+    public sealed class ChestModifyTests
+    {
+        private readonly byte[] _bytes = { 12, 0, 34, 1, 0, 4, 0, 8, 3, 0, 50, 0 };
+
+        [Theory]
+        [InlineData(ChestModification.PlaceChest)]
+        [InlineData(ChestModification.DestroyChest)]
+        [InlineData(ChestModification.PlaceContainer)]
+        [InlineData(ChestModification.DestroyContainer)]
+        public void Modification_Set_Get(ChestModification modification)
+        {
+            var packet = new ChestModify();
+
+            packet.Modification = modification;
+
+            Assert.Equal(modification, packet.Modification);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new ChestModify();
+
+            packet.X = 50;
+
+            Assert.Equal(50, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new ChestModify();
+
+            packet.Y = 50;
+
+            Assert.Equal(50, packet.Y);
+        }
+
+        [Fact]
+        public void Style_Set_Get()
+        {
+            var packet = new ChestModify();
+
+            packet.Style = 1234;
+
+            Assert.Equal(1234, packet.Style);
+        }
+
+        [Fact]
+        public void ChestIndex_Set_Get()
+        {
+            var packet = new ChestModify();
+
+            packet.ChestIndex = 10;
+
+            Assert.Equal(10, packet.ChestIndex);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<ChestModify>(_bytes, PacketContext.Server);
+
+            Assert.Equal(ChestModification.DestroyChest, packet.Modification);
+            Assert.Equal(1024, packet.X);
+            Assert.Equal(2048, packet.Y);
+            Assert.Equal(3, packet.Style);
+            Assert.Equal(50, packet.ChestIndex);
+        }
+    }
+}
