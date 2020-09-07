04499e25e0a5099b809e076776b25474db3287db
ivanbiljan <ssbiljan@gmail.com>
2020-09-07 12:59:36 +0200

    Implement NpcHome

---
+++ src/Orion.Core/Packets/Npcs/NpcHome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent to set an NPC's home.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 7)]
+    public struct NpcHome : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes;
+
+        /// <summary>
+        /// Gets or sets the NPC index.
+        /// </summary>
+        [field: FieldOffset(0)] public short NpcIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the home's X tile coordinate.
+        /// </summary>
+        [field: FieldOffset(2)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the home's Y tile coordinate.
+        /// </summary>
+        [field: FieldOffset(4)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the NPC is homeless.
+        /// </summary>
+        [field: FieldOffset(6)] public bool IsHomeless { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcHome;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 7);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 7);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcHomeTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    public sealed class NpcHomeTests
+    {
+        private readonly byte[] _bytes = { 10, 0, 60, 150, 0, 50, 0, 100, 0, 1 };
+
+        [Fact]
+        public void NpcIndex_Set_Get()
+        {
+            var packet = new NpcHome();
+
+            packet.NpcIndex = 199;
+
+            Assert.Equal(199, packet.NpcIndex);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new NpcHome();
+
+            packet.X = 100;
+
+            Assert.Equal(100, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new NpcHome();
+
+            packet.Y = 100;
+
+            Assert.Equal(100, packet.Y);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void IsHomeless_Set_Get(bool isHomeless)
+        {
+            var packet = new NpcHome();
+
+            packet.IsHomeless = isHomeless;
+
+            Assert.Equal(isHomeless, packet.IsHomeless);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<NpcHome>(_bytes, PacketContext.Server);
+
+            Assert.Equal(150, packet.NpcIndex);
+            Assert.Equal(50, packet.X);
+            Assert.Equal(100, packet.Y);
+            Assert.True(packet.IsHomeless);
+        }
+    }
+}
