7876bd37c24dd01a38bf3a6e24ac4783013ff146
ivanbiljan <ssbiljan@gmail.com>
2020-09-07 02:08:10 +0200

    Implement PlayerMusic

---
+++ src/Orion.Core/Packets/Players/PlayerMusic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent from server to clients to play notes.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 5)]
+    public struct PlayerMusic : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the note.
+        /// </summary>
+        [field: FieldOffset(1)] public float Note { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerMusic;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 5);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 5);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerMusicTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    public sealed class PlayerMusicTests
+    {
+        private readonly byte[] _bytes = { 8, 0, 58, 250, 0, 0, 0, 0 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerMusic();
+
+            packet.PlayerIndex = 1;
+
+            Assert.Equal(1, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void Note_Set_Get()
+        {
+            var packet = new PlayerMusic();
+
+            packet.Note = 3.33333F;
+
+            Assert.Equal(3.33333F, packet.Note);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<PlayerMusic>(_bytes, PacketContext.Server);
+
+            Assert.Equal(250, packet.PlayerIndex);
+            Assert.Equal(0, packet.Note);
+        }
+    }
+}
