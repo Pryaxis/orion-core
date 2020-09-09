01f16c123300fcbde49e0cdd156ea52e7d0d8395
ivanbiljan <ssbiljan@gmail.com>
2020-09-09 03:17:55 +0200

    Complete WorldInfo layout. Tests are still TODO

--- src/Orion.Core/Packets/World/WorldInfo.cs
+++ src/Orion.Core/Packets/World/WorldInfo.cs
@@ -34,33 +34,6 @@ namespace Orion.Core.Packets.World
         [field: FieldOffset(0)] public int Time { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether it is currently day time.
-        /// </summary>
-        public bool IsDayTime
-        {
-            get => _solarFlags[0];
-            set => _solarFlags[0] = value;
-        }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether it is currently blood moon.
-        /// </summary>
-        public bool IsBloodMoon
-        {
-            get => _solarFlags[1];
-            set => _solarFlags[1] = value;
-        }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether an eclipse is happening.
-        /// </summary>
-        public bool IsEclipse
-        {
-            get => _solarFlags[2];
-            set => _solarFlags[2] = value;
-        }
-
-        /// <summary>
         /// Gets or sets the moon phase.
         /// </summary>
         [field: FieldOffset(5)] public MoonPhase MoonPhase { get; set; }
@@ -274,9 +247,8 @@ namespace Orion.Core.Packets.World
         /// </summary>
         [field: FieldOffset(73)] public byte[] CaveBackgroundStyles { get; } = new byte[3];
 
-        // TODO: Look into this. Seems its only used 
         /// <summary>
-        /// Gets the world area style variations. Used for tree effects and background rendering.
+        /// Gets the current style variation for each world area. Used for tree effects and background rendering.
         /// </summary>
         [field: FieldOffset(77)] public byte[] AreaStyleVariation { get; } = new byte[13];
 
@@ -284,6 +256,83 @@ namespace Orion.Core.Packets.World
         /// Gets or sets the rain intensity. Values are [0, 1].
         /// </summary>
         [field: FieldOffset(90)] public float RainIntensity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Copper tier.
+        /// </summary>
+        [field: FieldOffset(101)] public short CopperTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Iron tier.
+        /// </summary>
+        [field: FieldOffset(103)] public short IronTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Silver tier.
+        /// </summary>
+        [field: FieldOffset(105)] public short SilverTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Gold tier.
+        /// </summary>
+        [field: FieldOffset(107)] public short GoldTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cobalt tier.
+        /// </summary>
+        [field: FieldOffset(109)] public short CobaltTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Mythril tier.
+        /// </summary>
+        [field: FieldOffset(111)] public short MythrilTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Adamantite tier.
+        /// </summary>
+        [field: FieldOffset(113)] public short AdamantiteTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current invasion type.
+        /// </summary>
+        [field: FieldOffset(115)] public sbyte InvasionType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lobby ID.
+        /// </summary>
+        [field: FieldOffset(116)] public ulong LobbyId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sandstorm severity.
+        /// </summary>
+        [field: FieldOffset(118)] public float SandstormSeverity { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether it is currently day time.
+        /// </summary>
+        public bool IsDayTime
+        {
+            get => _solarFlags[0];
+            set => _solarFlags[0] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether it is currently blood moon.
+        /// </summary>
+        public bool IsBloodMoon
+        {
+            get => _solarFlags[1];
+            set => _solarFlags[1] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether an eclipse is happening.
+        /// </summary>
+        public bool IsEclipse
+        {
+            get => _solarFlags[2];
+            set => _solarFlags[2] = value;
+        }
 
         /// <summary>
         /// Gets or sets a value indicating whether at least one Shadow Orb was smashed.
@@ -364,6 +413,420 @@ namespace Orion.Core.Packets.World
         {
             get => _worldFlags2[0];
             set => _worldFlags2[0] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Twins were defeated.
+        /// </summary>
+        public bool IsMechanicalEyeDefeated
+        {
+            get => _worldFlags2[1];
+            set => _worldFlags2[1] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Skeletron Prime has been defeated.
+        /// </summary>
+        public bool IsSkeletronPrimeDefeated
+        {
+            get => _worldFlags2[2];
+            set => _worldFlags2[2] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether any mechanical boss has been defeated.
+        /// </summary>
+        public bool DownedAnyMechanicalBoss
+        {
+            get => _worldFlags2[3];
+            set => _worldFlags2[3] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether clouds are rendered when drawing the background.
+        /// </summary>
+        public bool IsCloudBackgroundActive
+        {
+            get => _worldFlags2[4];
+            set => _worldFlags2[4] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Crimson is the world evil.
+        /// </summary>
+        public bool IsCrimson
+        {
+            get => _worldFlags2[5];
+            set => _worldFlags2[5] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a pumpkin moon event is happening.
+        /// </summary>
+        public bool IsPumpkinMoon
+        {
+            get => _worldFlags2[6];
+            set => _worldFlags2[6] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a snow moon event is happening.
+        /// </summary>
+        public bool IsSnowMoon
+        {
+            get => _worldFlags2[7];
+            set => _worldFlags2[7] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether time is fast forwarded.
+        /// </summary>
+        public bool FastForwardTime
+        {
+            get => _worldFlags3[1]; // This is not a typo. The first bit is ignored.
+            set => _worldFlags3[1] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether it is raining slime.
+        /// </summary>
+        public bool IsRainingSlime
+        {
+            get => _worldFlags3[2];
+            set => _worldFlags3[2] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Slime King has been defeated.
+        /// </summary>
+        public bool IsSlimeKingDefeated
+        {
+            get => _worldFlags3[3];
+            set => _worldFlags3[3] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Queen Bee has been defeated.
+        /// </summary>
+        public bool IsQueenBeeDefeated
+        {
+            get => _worldFlags3[4];
+            set => _worldFlags3[4] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Duke Fishron has been defeated.
+        /// </summary>
+        public bool IsFishronDefeated
+        {
+            get => _worldFlags3[5];
+            set => _worldFlags3[5] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Martians have been defeated.
+        /// </summary>
+        public bool AreMartiansDefeated
+        {
+            get => _worldFlags3[6];
+            set => _worldFlags3[6] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Ancient Cultists have been defeated.
+        /// </summary>
+        public bool AreCultistsDefeated
+        {
+            get => _worldFlags3[7];
+            set => _worldFlags3[7] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Moon Lord has been defeated.
+        /// </summary>
+        public bool IsMoonLordDefeated
+        {
+            get => _worldFlags4[0];
+            set => _worldFlags4[0] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Haloween King has been defeated.
+        /// </summary>
+        public bool IsHalloweenKingDefeated
+        {
+            get => _worldFlags4[1];
+            set => _worldFlags4[1] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Haloween Tree has been defeated.
+        /// </summary>
+        public bool IsHalloweenTreeDefeated
+        {
+            get => _worldFlags4[2];
+            set => _worldFlags4[2] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Christmas Queen has been defeated.
+        /// </summary>
+        public bool IsChristmasQueenDefeated
+        {
+            get => _worldFlags4[3];
+            set => _worldFlags4[3] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Christmas Santank has been defeated.
+        /// </summary>
+        public bool IsChristmasSantankDefeated
+        {
+            get => _worldFlags4[4];
+            set => _worldFlags4[4] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Christmas Tree has been defeated.
+        /// </summary>
+        public bool IsChristmasTreeDefeated
+        {
+            get => _worldFlags4[5];
+            set => _worldFlags4[5] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Golem has been defeated.
+        /// </summary>
+        public bool IsGolemDefeated
+        {
+            get => _worldFlags4[6];
+            set => _worldFlags4[6] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a birthday party is being thrown.
+        /// </summary>
+        public bool IsManualBirthdayParty
+        {
+            get => _worldFlags4[7];
+            set => _worldFlags4[7] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Pirates have been defeated.
+        /// </summary>
+        public bool ArePiratesDefeated
+        {
+            get => _worldFlags5[0];
+            set => _worldFlags5[0] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Frost Moon has been defeated.
+        /// </summary>
+        public bool IsFrostMoonDefeated
+        {
+            get => _worldFlags5[1];
+            set => _worldFlags5[1] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Goblins have been defeated.
+        /// </summary>
+        public bool AreGoblinsDefeated
+        {
+            get => _worldFlags5[2];
+            set => _worldFlags5[2] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a sandstorm is happening.
+        /// </summary>
+        public bool IsSandstormHappening
+        {
+            get => _worldFlags5[3];
+            set => _worldFlags5[3] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the DD2 event is happening.
+        /// </summary>
+        public bool IsDD2EventHappening
+        {
+            get => _worldFlags5[4];
+            set => _worldFlags5[4] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the first DD2 invasion has been defeated.
+        /// </summary>
+        public bool IsFirstDD2InvasionDefeated
+        {
+            get => _worldFlags5[5];
+            set => _worldFlags5[5] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the second DD2 invasion has been defeated.
+        /// </summary>
+        public bool IsSecondDD2InvasionDefeated
+        {
+            get => _worldFlags5[6];
+            set => _worldFlags5[6] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the third DD2 invasion has been defeated.
+        /// </summary>
+        public bool IsThirdDD2InvasionDefeated
+        {
+            get => _worldFlags5[7];
+            set => _worldFlags5[7] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Combat Book has been used.
+        /// </summary>
+        public bool WasCombatBookUsed
+        {
+            get => _worldFlags6[0];
+            set => _worldFlags6[0] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the server triggered the lantern event.
+        /// </summary>
+        public bool IsManualLanternNight
+        {
+            get => _worldFlags6[1];
+            set => _worldFlags6[1] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Solar Tower has been downed.
+        /// </summary>
+        public bool IsSolarTowerDowned
+        {
+            get => _worldFlags6[2];
+            set => _worldFlags6[2] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Vortex Tower has been downed.
+        /// </summary>
+        public bool IsVortexTowerDowned
+        {
+            get => _worldFlags6[3];
+            set => _worldFlags6[3] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Nebula Tower has been downed.
+        /// </summary>
+        public bool IsNebulaTowerDowned
+        {
+            get => _worldFlags6[4];
+            set => _worldFlags6[4] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Stardust Tower has been downed.
+        /// </summary>
+        public bool IsStardustTowerDowned
+        {
+            get => _worldFlags6[5];
+            set => _worldFlags6[5] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Halloween should be forced for the day.
+        /// </summary>
+        public bool ForceHalloweenForToday
+        {
+            get => _worldFlags6[6];
+            set => _worldFlags6[6] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Christmas should be forced for the day.
+        /// </summary>
+        public bool ForceChristmasForToday
+        {
+            get => _worldFlags6[7];
+            set => _worldFlags6[7] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a cat was bought.
+        /// </summary>
+        public bool WasCatBought
+        {
+            get => _worldFlags7[0];
+            set => _worldFlags7[0] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a dog was bought.
+        /// </summary>
+        public bool WasDogBought
+        {
+            get => _worldFlags7[1];
+            set => _worldFlags7[1] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a bunny was bought.
+        /// </summary>
+        public bool WasBunnyBought
+        {
+            get => _worldFlags7[2];
+            set => _worldFlags7[2] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether cake is free.
+        /// </summary>
+        public bool IsFreeCake
+        {
+            get => _worldFlags7[3];
+            set => _worldFlags7[3] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the world is "drunk".
+        /// </summary>
+        public bool IsDrunkWorld
+        {
+            get => _worldFlags7[4];
+            set => _worldFlags7[4] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Empress of Light has been defeated.
+        /// </summary>
+        public bool IsEmpressOfLightDefeated
+        {
+            get => _worldFlags7[5];
+            set => _worldFlags7[5] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a vlaue indicating whether Queen Slime has been defeated.
+        /// </summary>
+        public bool IsQueenSlimeDefeated
+        {
+            get => _worldFlags7[6];
+            set => _worldFlags7[6] = value;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether <c>getGoodWorldGen</c> was used upon world generation. Aims to drastically increase the game's difficulty.
+        /// </summary>
+        public bool IsForTheWorthyWorld
+        {
+            get => _worldFlags7[7];
+            set => _worldFlags7[7] = value;
         }
 
         PacketId IPacket.Id => PacketId.WorldInfo;
