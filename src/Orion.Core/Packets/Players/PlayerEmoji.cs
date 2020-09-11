988cffdce6fe8276a6802f26bdfe6400f18fa3cf
ivanbiljan <ssbiljan@gmail.com>
2020-09-12 00:19:36 +0200

    Implement PlayerEmoji

---
+++ src/Orion.Core/Packets/Players/PlayerEmoji.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet used to send an emoji.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 2)]
+    public struct PlayerEmoji : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes;
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        [field: FieldOffset(0)]
+        public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the emoji ID.
+        /// </summary>
+        [field: FieldOffset(1)]
+        public byte EmojiId
+        {
+            get;
+            set;
+        }
+
+        PacketId IPacket.Id => PacketId.PlayerEmoji;
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 2);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 2);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerEmojiTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    public sealed class PlayerEmojiTests
+    {
+        private readonly byte[] _bytes = { 5, 0, 120, 4, 5 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerEmoji();
+
+            packet.PlayerIndex = 1;
+
+            Assert.Equal(1, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void EmojiId_Set_Get()
+        {
+            var packet = new PlayerEmoji();
+
+            packet.EmojiId = 1;
+
+            Assert.Equal(1, packet.EmojiId);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<PlayerEmoji>(_bytes, PacketContext.Server);
+
+            Assert.Equal(4, packet.PlayerIndex);
+            Assert.Equal(5, packet.EmojiId);
+        }
+    }
+}
