ff8d0da327b817bac00cd922f27428175a18c805
ivanbiljan <ssbiljan@gmail.com>
2020-09-12 00:10:44 +0200

    Implement ServerConnected

---
+++ src/Orion.Core/Packets/Players/ServerConnected.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent from the server to the client upon initial spawn. Instructs the client to set the UI scale and
+    /// process world information.
+    /// </summary>
+    public struct ServerConnected : IPacket
+    {
+        PacketId IPacket.Id => PacketId.ServerConnected;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => 0;
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => 0;
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/ServerConnectedTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    public sealed class ServerConnectedTests
+    {
+        private readonly byte[] _bytes = { 3, 0, 129 };
+
+        [Fact]
+        public void Read()
+        {
+            var _ = TestUtils.ReadPacket<ServerConnected>(_bytes, PacketContext.Server);
+        }
+    }
+}
