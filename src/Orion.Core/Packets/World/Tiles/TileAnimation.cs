bd4d47799cdfa0f78b95d32a4ef9fc8523468aaf
ivanbiljan <ssbiljan@gmail.com>
2020-09-12 22:26:20 +0200

    Impelement TileAnimation

---
+++ src/Orion.Core/Packets/World/Tiles/TileAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World.Tiles
+{
+    /// <summary>
+    /// A packet sent to set a tile animation.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 8)]
+    public struct TileAnimation : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes;
+
+        /// <summary>
+        /// Gets or sets the animation type.
+        /// </summary>
+        [field: FieldOffset(0)] public short AnimationType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tile type.
+        /// </summary>
+        [field: FieldOffset(2)] public short TileType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tile's X coordinate.
+        /// </summary>
+        [field: FieldOffset(4)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tile's Y coordinate.
+        /// </summary>
+        [field: FieldOffset(6)] public short Y { get; set; }
+
+        PacketId IPacket.Id => PacketId.TileAnimation;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 8);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 8);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/Tiles/TileAnimationTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReLogic.Content;
+using Xunit;
+
+namespace Orion.Core.Packets.World.Tiles
+{
+    public sealed class TileAnimationTests
+    {
+        private readonly byte[] _bytes = { 11, 0, 77, 1, 0, 2, 0, 3, 0, 4, 0 };
+
+        [Fact]
+        public void AnimationType_Set_Get()
+        {
+            var packet = new TileAnimation();
+
+            packet.AnimationType = 1;
+
+            Assert.Equal(1, packet.AnimationType);
+        }
+
+        [Fact]
+        public void TileType_Set_Get()
+        {
+            var packet = new TileAnimation();
+
+            packet.TileType = 1;
+
+            Assert.Equal(1, packet.TileType);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new TileAnimation();
+
+            packet.X = 1;
+
+            Assert.Equal(1, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new TileAnimation();
+
+            packet.Y = 1;
+
+            Assert.Equal(1, packet.Y);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<TileAnimation>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.AnimationType);
+            Assert.Equal(2, packet.TileType);
+            Assert.Equal(3, packet.X);
+            Assert.Equal(4, packet.Y);
+        }
+    }
+}
