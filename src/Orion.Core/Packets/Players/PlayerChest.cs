8254aa1c113bb191aa5895a8b3479d84950308a9
ivanbiljan <ssbiljan@gmail.com>
2020-09-12 23:41:31 +0200

    Implement PlayerChest

---
+++ src/Orion.Core/Packets/Players/PlayerChest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to set a player's selected chest index.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 3)]
+    public struct PlayerChest : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+        
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chest index.
+        /// </summary>
+        [field: FieldOffset(1)] public short ChestIndex { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerChest;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 3);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 3);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerChestTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    public sealed class PlayerChestTests
+    {
+        private readonly byte[] _bytes = { 6, 0, 80, 1, 2, 0 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerChest();
+
+            packet.PlayerIndex = 1;
+
+            Assert.Equal(1, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void ChestIndex_Set_Get()
+        {
+            var packet = new PlayerChest();
+
+            packet.ChestIndex = 1;
+
+            Assert.Equal(1, packet.ChestIndex);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<PlayerChest>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.PlayerIndex);
+            Assert.Equal(2, packet.ChestIndex);
+        }
+    }
+}
