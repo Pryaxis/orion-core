fbd957f05204d3348a2c7c8d736435b48091df1e
ivanbiljan <ssbiljan@gmail.com>
2020-09-12 23:15:05 +0200

    Implement ObjectPlaceTests

---
+++ src/Orion.Core/Packets/World/ObjectPlace.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet used to send object placements.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 11)]
+    public struct ObjectPlace : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the X coordinate.
+        /// </summary>
+        [field: FieldOffset(0)] public short X { get; set; }
+
+        /// <summary>
+        /// Gest or sets the Y coordinate.
+        /// </summary>
+        [field: FieldOffset(2)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of tile.
+        /// </summary>
+        [field: FieldOffset(4)] public short TileType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the style.
+        /// </summary>
+        [field: FieldOffset(6)] public short Style { get; set; }
+
+        /// <summary>
+        /// Gets or sets the alternate tile style.
+        /// </summary>
+        [field: FieldOffset(8)] public byte AlternateStyle { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value used when calculating a random placement style.
+        /// </summary>
+        [field: FieldOffset(9)] public sbyte RandomStyleModifier { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the tile is placed facing the right direction.
+        /// </summary>
+        [field: FieldOffset(10)] public bool IsRightDirection { get; set; }
+
+        PacketId IPacket.Id => PacketId.ObjectPlace;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 11);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 11);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/ObjectPlaceTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    public sealed class ObjectPlaceTests
+    {
+        private readonly byte[] _bytes = { 14, 0, 79, 1, 0, 2, 0, 3, 0, 4, 0, 5, 6, 1 };
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new ObjectPlace();
+
+            packet.X = 1;
+
+            Assert.Equal(1, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new ObjectPlace();
+
+            packet.Y = 1;
+
+            Assert.Equal(1, packet.Y);
+        }
+
+        [Fact]
+        public void TileType_Set_Get()
+        {
+            var packet = new ObjectPlace();
+
+            packet.TileType = 1;
+
+            Assert.Equal(1, packet.TileType);
+        }
+
+        [Fact]
+        public void Style_Set_Get()
+        {
+            var packet = new ObjectPlace();
+
+            packet.Style = 1;
+
+            Assert.Equal(1, packet.Style);
+        }
+
+        [Fact]
+        public void AlternateStyle_Set_Get()
+        {
+            var packet = new ObjectPlace();
+
+            packet.AlternateStyle = 1;
+
+            Assert.Equal(1, packet.AlternateStyle);
+        }
+
+        [Fact]
+        public void RandomStyleModifier_Set_Get()
+        {
+            var packet = new ObjectPlace();
+
+            packet.RandomStyleModifier = 1;
+
+            Assert.Equal(1, packet.RandomStyleModifier);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void IsRightDirection_Set_Get(bool value)
+        {
+            var packet = new ObjectPlace();
+
+            packet.IsRightDirection = value;
+
+            Assert.Equal(value, packet.IsRightDirection);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<ObjectPlace>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.X);
+            Assert.Equal(2, packet.Y);
+            Assert.Equal(3, packet.TileType);
+            Assert.Equal(4, packet.Style);
+            Assert.Equal(5, packet.AlternateStyle);
+            Assert.Equal(6, packet.RandomStyleModifier);
+            Assert.True(packet.IsRightDirection);
+        }
+    }
+}
