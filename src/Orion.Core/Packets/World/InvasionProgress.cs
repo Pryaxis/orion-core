0a2fd90320c8a4491bde7c08d14c1866701f03d3
ivanbiljan <ssbiljan@gmail.com>
2020-09-12 22:37:55 +0200

    Implement InvasionProgress

---
+++ src/Orion.Core/Packets/World/InvasionProgress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent to report invasion progress.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 10)]
+    public struct InvasionProgress : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the current progress.
+        /// </summary>
+        [field: FieldOffset(0)] public int CurrentProgress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum progress.
+        /// </summary>
+        [field: FieldOffset(4)] public int MaxProgress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the invasion icon.
+        /// </summary>
+        [field: FieldOffset(8)] public sbyte Icon { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current wave.
+        /// </summary>
+        [field: FieldOffset(9)] public sbyte CurrentWave { get; set; }
+
+        PacketId IPacket.Id => PacketId.InvasionProgress;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 10);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 10);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/InvasionProgressTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    public sealed class InvasionProgressTests
+    {
+        private readonly byte[] _bytes = { 13, 0, 78, 5, 0, 0, 0, 10, 0, 0, 0, 0, 5 };
+
+        [Fact]
+        public void CurrentProgress_Set_Get()
+        {
+            var packet = new InvasionProgress();
+
+            packet.CurrentProgress = 1;
+
+            Assert.Equal(1, packet.CurrentProgress);
+        }
+
+        [Fact]
+        public void MaxProgress_Set_Get()
+        {
+            var packet = new InvasionProgress();
+
+            packet.MaxProgress = 1;
+
+            Assert.Equal(1, packet.MaxProgress);
+        }
+
+        [Fact]
+        public void Icon_Set_Get()
+        {
+            var packet = new InvasionProgress();
+
+            packet.Icon = 1;
+
+            Assert.Equal(1, packet.Icon);
+        }
+
+        [Fact]
+        public void CurrentWave_Set_Get()
+        {
+            var packet = new InvasionProgress();
+
+            packet.CurrentWave = 1;
+
+            Assert.Equal(1, packet.CurrentWave);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<InvasionProgress>(_bytes, PacketContext.Server);
+
+            Assert.Equal(5, packet.CurrentProgress);
+            Assert.Equal(10, packet.MaxProgress);
+            Assert.Equal(0, packet.Icon);
+            Assert.Equal(5, packet.CurrentWave);
+        }
+    }
+}
