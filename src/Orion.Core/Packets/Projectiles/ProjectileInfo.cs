8ce63509f34ecab2ebfc3f7c66d7931eb6a832d3
ivanbiljan <ssbiljan@gmail.com>
2020-09-13 19:48:48 +0200

    Implement ProjectileInfo

---
+++ src/Orion.Core/Packets/Projectiles/ProjectileInfo.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Packets.DataStructures;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Projectiles
+{
+    /// <summary>
+    /// A packet sent to update projectiles.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 42)]
+    public struct ProjectileInfo : IPacket
+    {
+        private const int MaxAi = 2;
+
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+        [FieldOffset(32)] private byte _bytes2; // Used to obtain an interiror reference
+        [FieldOffset(34)] private byte _bytes3; // Used to obtain an interiror reference
+        [FieldOffset(38)] private byte _bytes4; // Used to obtain an interiror reference
+        [FieldOffset(40)] private byte _bytes5; // Used to obtain an interiror reference
+        [FieldOffset(21)] private Flags8 _flags;
+        [FieldOffset(24)] private float[]? _ai;
+
+        /// <summary>
+        /// Gets or sets the identity, i.e, the projectile index.
+        /// </summary>
+        [field: FieldOffset(0)] public short Identity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X position.
+        /// </summary>
+        [field: FieldOffset(2)] public float X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y position.
+        /// </summary>
+        [field: FieldOffset(6)] public float Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the velocity X.
+        /// </summary>
+        [field: FieldOffset(10)] public float VelocityX { get; set; }
+
+        /// <summary>
+        /// Gets or sets the velocity Y.
+        /// </summary>
+        [field: FieldOffset(14)] public float VelocityY { get; set; }
+
+        /// <summary>
+        /// Gets or sets the owner index.
+        /// </summary>
+        [field: FieldOffset(18)] public byte OwnerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the projectile type.
+        /// </summary>
+        [field: FieldOffset(19)] public short Type { get; set; }
+
+        /// <summary>
+        /// Gets additional information.
+        /// </summary>
+        public float[] AdditionalInformation => _ai ??= new float[MaxAi];
+
+        /// <summary>
+        /// Gets or sets the damage.
+        /// </summary>
+        [field: FieldOffset(32)] public short Damage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the knockback.
+        /// </summary>
+        [field: FieldOffset(34)] public float Knockback { get; set; }
+
+        /// <summary>
+        /// Gets or sets the original damage.
+        /// </summary>
+        [field: FieldOffset(38)] public short OriginalDamage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UUID.
+        /// </summary>
+        [field: FieldOffset(40)] public short Uuid { get; set; }
+
+        PacketId IPacket.Id => PacketId.ProjectileInfo;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Read(ref _bytes, 22);
+            for (var i = 0; i < AdditionalInformation.Length; ++i)
+            {
+                if (!_flags[i])
+                {
+                    continue;
+                }
+
+                Unsafe.CopyBlockUnaligned(ref Unsafe.Add(ref Unsafe.As<float, byte>(ref AdditionalInformation[0]), i * 4), ref span[length], 4);
+                length += 4;
+            }
+
+            if (_flags[4])
+            {
+                length += span[length..].Read(ref _bytes2, 2);
+            }
+
+            if (_flags[5])
+            {
+                length += span[length..].Read(ref _bytes3, 4);
+            }
+
+            if (_flags[6])
+            {
+                length += span[length..].Read(ref _bytes4, 2);
+            }
+
+            if (_flags[7])
+            {
+                length += span[length..].Read(ref _bytes5, 2);
+            }
+
+            return length;
+        }
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Write(ref _bytes, 22);
+            var aiReference = Unsafe.As<float, byte>(ref AdditionalInformation[0]);
+            for (var i = 0; i < AdditionalInformation.Length; ++i)
+            {
+                if (!_flags[i])
+                {
+                    continue;
+                }
+
+                Unsafe.CopyBlockUnaligned(ref span[length], ref Unsafe.Add(ref Unsafe.As<float, byte>(ref AdditionalInformation[0]), i * 4), 4);
+                length += 4;
+            }
+
+            if (Damage > 0)
+            {
+                length += span[length..].Write(ref _bytes2, 2);
+            }
+
+            if (Knockback > 0)
+            {
+                length += span[length..].Write(ref _bytes3, 4);
+            }
+
+            if (OriginalDamage > 0)
+            {
+                length += span[length..].Write(ref _bytes4, 2);
+            }
+
+            if (Uuid > 0)
+            {
+                length += span[length..].Write(ref _bytes5, 2);
+            }
+
+            return length;
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Projectiles/ProjectileInfoTests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Projectiles
+{
+    public sealed class ProjectileInfoTests
+    {
+        private readonly byte[] _bytes =
+        {
+            39, 0, 27, 1, 0, 0, 0, 0, 64, 0, 0, 64, 64, 0, 0, 128, 64, 0, 0, 160, 64, 6, 7, 0, 241, 0, 0, 128, 63, 8, 0, 0, 0, 128, 63, 9, 0, 10, 0
+        };
+
+
+        [Fact]
+        public void Identity_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.Identity = 5;
+
+            Assert.Equal(5, packet.Identity);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.X = 12345F;
+
+            Assert.Equal(12345F, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.Y = 67890F;
+
+            Assert.Equal(67890F, packet.Y);
+        }
+
+        [Fact]
+        public void VelocityX_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.VelocityX = 1;
+
+            Assert.Equal(1, packet.VelocityX);
+        }
+
+        [Fact]
+        public void VelocityY_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.VelocityY = 1;
+
+            Assert.Equal(1, packet.VelocityY);
+        }
+
+        [Fact]
+        public void OwnerIndex_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.OwnerIndex = 1;
+
+            Assert.Equal(1, packet.OwnerIndex);
+        }
+
+        [Fact]
+        public void Type_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.Type = 1;
+
+            Assert.Equal(1, packet.Type);
+        }
+
+        [Fact]
+        public void AdditionalInformation_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.AdditionalInformation[0] = 1;
+            packet.AdditionalInformation[1] = 2;
+
+            Assert.Equal(1, packet.AdditionalInformation[0]);
+            Assert.Equal(2, packet.AdditionalInformation[1]);
+        }
+
+        [Fact]
+        public void Damage_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.Damage = 1;
+
+            Assert.Equal(1, packet.Damage);
+        }
+
+        [Fact]
+        public void Knockback_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.Knockback = 1;
+
+            Assert.Equal(1, packet.Knockback);
+        }
+
+        [Fact]
+        public void OriginalDamage_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.OriginalDamage = 1;
+
+            Assert.Equal(1, packet.OriginalDamage);
+        }
+
+        [Fact]
+        public void Uuid_Set_Get()
+        {
+            var packet = new ProjectileInfo();
+
+            packet.Uuid = 1;
+
+            Assert.Equal(1, packet.Uuid);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<ProjectileInfo>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.Identity);
+            Assert.Equal(2F, packet.X);
+            Assert.Equal(3F, packet.Y);
+            Assert.Equal(4F, packet.VelocityX);
+            Assert.Equal(5F, packet.VelocityY);
+            Assert.Equal(6, packet.OwnerIndex);
+            Assert.Equal(7, packet.Type);
+            Assert.Equal(1F, packet.AdditionalInformation[0]);
+            Assert.Equal(8, packet.Damage);
+            Assert.Equal(1F, packet.Knockback);
+            Assert.Equal(9, packet.OriginalDamage);
+            Assert.Equal(10, packet.Uuid);
+        }
+    }
+}
