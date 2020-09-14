43326a32c5e66477dbc888838aec8d7d5177e183
ivanbiljan <ssbiljan@gmail.com>
2020-09-14 23:56:24 +0200

    Implement NpcImmunity

---
+++ src/Orion.Core/Packets/Npcs/NpcImmunity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent to set an NPC's immunity.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 9)]
+    public struct NpcImmunity : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+        [FieldOffset(3)] private byte _bytes2; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the NPC index.
+        /// </summary>
+        [field: FieldOffset(0)] public ushort NpcIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether immunity should be updated.
+        /// </summary>
+        [field: FieldOffset(2)] public bool ShouldUpdateImmunity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the immune time.
+        /// </summary>
+        [field: FieldOffset(3)] public int ImmuneTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the index of the player the NPC is immune to.
+        /// </summary>
+        [field: FieldOffset(7)] public short PlayerIndex { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcImmunity;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Read(ref _bytes, 3);
+            if (ShouldUpdateImmunity)
+            {
+                length += span[length..].Read(ref _bytes2, 6);
+            }
+
+            return length;
+        }
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Write(ref _bytes, 3);
+            if (ShouldUpdateImmunity)
+            {
+                length += span[length..].Write(ref _bytes2, 6);
+            }
+
+            return length;
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcImmunityTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    public sealed class NpcImmunityTests
+    {
+        private readonly byte[] _bytes = { 12, 0, 131, 5, 0, 1, 60, 0, 0, 0, 0, 0 };
+
+        [Fact]
+        public void NpcIndex_Set_Get()
+        {
+            var packet = new NpcImmunity();
+
+            packet.NpcIndex = 1;
+
+            Assert.Equal(1, packet.NpcIndex);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ShouldUpdateImmunity_Set_Get(bool value)
+        {
+            var packet = new NpcImmunity();
+
+            packet.ShouldUpdateImmunity = value;
+
+            Assert.Equal(value, packet.ShouldUpdateImmunity);
+        }
+
+        [Fact]
+        public void ImmuneTime_Set_Get()
+        {
+            var packet = new NpcImmunity();
+
+            packet.ImmuneTime = 1;
+
+            Assert.Equal(1, packet.ImmuneTime);
+        }
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new NpcImmunity();
+
+            packet.PlayerIndex = 1;
+
+            Assert.Equal(1, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<NpcImmunity>(_bytes, PacketContext.Server);
+
+            Assert.Equal(5, packet.NpcIndex);
+            Assert.True(packet.ShouldUpdateImmunity);
+            Assert.Equal(60, packet.ImmuneTime);
+            Assert.Equal(0, packet.PlayerIndex);
+        }
+    }
+}
