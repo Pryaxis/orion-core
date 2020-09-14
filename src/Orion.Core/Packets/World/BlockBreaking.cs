9fb7afacc2ad1f29f8a85f44fe3d5d4eea9b1e74
ivanbiljan <ssbiljan@gmail.com>
2020-09-14 22:17:19 +0200

    Implement BlockBreaking

---
+++ src/Orion.Core/Packets/World/BlockBreaking.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent to damage a tile.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 6)]
+    public struct BlockBreaking : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the index of the player breaking the block.
+        /// </summary>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the block's X coordinate.
+        /// </summary>
+        [field: FieldOffset(1)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or set sthe block's Y coordinate.
+        /// </summary>
+        [field: FieldOffset(3)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pick damage.
+        /// </summary>
+        [field: FieldOffset(5)] public byte PickDamage { get; set; }
+
+        PacketId IPacket.Id => PacketId.BlockBreaking;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 6);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 6);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/BlockBreakingTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    public sealed class BlockBreakingTests
+    {
+        private readonly byte[] _bytes = { 9, 0, 125, 1, 2, 0, 3, 0, 4 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new BlockBreaking();
+
+            packet.PlayerIndex = 1;
+
+            Assert.Equal(1, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new BlockBreaking();
+
+            packet.X = 1;
+
+            Assert.Equal(1, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new BlockBreaking();
+
+            packet.Y = 1;
+
+            Assert.Equal(1, packet.Y);
+        }
+
+        [Fact]
+        public void PickDamage_Set_Get()
+        {
+            var packet = new BlockBreaking();
+
+            packet.PickDamage = 1;
+
+            Assert.Equal(1, packet.PickDamage);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<BlockBreaking>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.PlayerIndex);
+            Assert.Equal(2, packet.X);
+            Assert.Equal(3, packet.Y);
+            Assert.Equal(4, packet.PickDamage);
+        }
+    }
+}
