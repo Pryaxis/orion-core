125d8cc86d157697649fd80a92296f8ab7075c48
ivanbiljan <ssbiljan@gmail.com>
2020-09-14 13:57:15 +0200

    Implement PlayerQuickStack

---
+++ src/Orion.Core/Packets/Players/PlayerQuickStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to quick stack an item.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 1)]
+    public struct PlayerQuickStack : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the slot that denotes the quick-stacked item.
+        /// </summary>
+        [field: FieldOffset(0)] public byte InventorySlot { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerQuickStack;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 1);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 1);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerQuickStackTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    public sealed class PlayerQuickStackTests
+    {
+        private readonly byte[] _bytes = { 4, 0, 85, 1 };
+
+        [Fact]
+        public void InventorySlot_Set_Get()
+        {
+            var packet = new PlayerQuickStack();
+
+            packet.InventorySlot = 1;
+
+            Assert.Equal(1, packet.InventorySlot);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<PlayerQuickStack>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.InventorySlot);
+        }
+    }
+}
