a3e2750489a62e99f2b6310df94229a16d43a402
ivanbiljan <ssbiljan@gmail.com>
2020-09-14 22:07:37 +0200

    Implement PillarShieldStrengths

---
+++ src/Orion.Core/Packets/World/PillarShieldStrengths.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent to update pillar shield strengths.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 8)]
+    public struct PillarShieldStrengths : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the Solar pillar's shield strength.
+        /// </summary>
+        [field: FieldOffset(0)] public ushort SolarShieldStrength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Vortex pillar's shield strength.
+        /// </summary>
+        [field: FieldOffset(2)] public ushort VortexShieldStrength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Nebula pillar's shield strength.
+        /// </summary>
+        [field: FieldOffset(4)] public ushort NebulaShieldStrength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Stardust pillar's shield strength.
+        /// </summary>
+        [field: FieldOffset(6)] public ushort StardustShieldStrength { get; set; }
+
+        PacketId IPacket.Id => PacketId.PillarShieldStrengths;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 8);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 8);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/PillarShieldStrengthsTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    public sealed class PillarShieldStrengthsTests
+    {
+        private readonly byte[] _bytes = { 11, 0, 101, 1, 0, 2, 0, 3, 0, 4, 0 };
+
+        [Fact]
+        public void SolarShieldStrength_Set_Get()
+        {
+            var packet = new PillarShieldStrengths();
+
+            packet.SolarShieldStrength = 1;
+
+            Assert.Equal(1, packet.SolarShieldStrength);
+        }
+
+        [Fact]
+        public void VortexShieldStrength_Set_Get()
+        {
+            var packet = new PillarShieldStrengths();
+
+            packet.VortexShieldStrength = 1;
+
+            Assert.Equal(1, packet.VortexShieldStrength);
+        }
+
+        [Fact]
+        public void NebulaShieldStrength_Set_Get()
+        {
+            var packet = new PillarShieldStrengths();
+
+            packet.NebulaShieldStrength = 1;
+
+            Assert.Equal(1, packet.NebulaShieldStrength);
+        }
+
+        [Fact]
+        public void StardustShieldStrength_Set_Get()
+        {
+            var packet = new PillarShieldStrengths();
+
+            packet.StardustShieldStrength = 1;
+
+            Assert.Equal(1, packet.StardustShieldStrength);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<PillarShieldStrengths>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.SolarShieldStrength);
+            Assert.Equal(2, packet.VortexShieldStrength);
+            Assert.Equal(3, packet.NebulaShieldStrength);
+            Assert.Equal(4, packet.StardustShieldStrength);
+        }
+    }
+}
