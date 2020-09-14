84144d5331a103b91f0548cc4374c0df4fb7840a
ivanbiljan <ssbiljan@gmail.com>
2020-09-14 23:25:04 +0200

    Implement NpcRemoveRevenge

---
+++ src/Orion.Core/Packets/Npcs/NpcRemoveRevenge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent to remove an NPC's revenge.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 4)]
+    public struct NpcRemoveRevenge : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the unique ID used to identify the revenge marker.
+        /// </summary>
+        [field: FieldOffset(0)] public int UniqueId { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcRemoveRevenge;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 4);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 4);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcRemoveRevengeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    public sealed class NpcRemoveRevengeTests
+    {
+        private readonly byte[] _bytes = { 7, 0, 127, 1, 0, 0, 0 };
+
+        [Fact]
+        public void UniqueId_Set_Get()
+        {
+            var packet = new NpcRemoveRevenge();
+
+            packet.UniqueId = 1;
+
+            Assert.Equal(1, packet.UniqueId);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<NpcRemoveRevenge>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.UniqueId);
+        }
+    }
+}
