b6ad3b8fb84d8e66d5484723b001a987b5ac924f
ivanbiljan <ssbiljan@gmail.com>
2020-09-17 21:26:57 +0200

    Implement ProjectileRemoveIndex

---
+++ src/Orion.Core/Packets/Projectiles/ProjectileRemoveIndex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Projectiles
+{
+    /// <summary>
+    /// A packet sent to remove intersecting portals.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 3)]
+    public struct ProjectileRemoveIndex : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the owner index.
+        /// </summary>
+        [field: FieldOffset(0)] public ushort OwnerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the portal form.
+        /// </summary>
+        [field: FieldOffset(2)] public byte PortalForm { get; set; }
+
+        PacketId IPacket.Id => PacketId.ProjectileRemoveIndex;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 3);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 3);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Projectiles/ProjectileRemoveIndexTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Projectiles
+{
+    public sealed class ProjectileRemoveIndexTests
+    {
+        private readonly byte[] _bytes = { 6, 0, 95, 1, 0, 2 };
+
+        [Fact]
+        public void OwnerIndex_Set_Get()
+        {
+            var packet = new ProjectileRemoveIndex();
+
+            packet.OwnerIndex = 1;
+
+            Assert.Equal(1, packet.OwnerIndex);
+        }
+
+        [Fact]
+        public void PortalForm_Set_Get()
+        {
+            var packet = new ProjectileRemoveIndex();
+
+            packet.PortalForm = 1;
+
+            Assert.Equal(1, packet.PortalForm);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<ProjectileRemoveIndex>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.OwnerIndex);
+            Assert.Equal(2, packet.PortalForm);
+        }
+    }
+}
