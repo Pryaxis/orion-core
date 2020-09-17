23f976567610096f24ac6403deac2c3299d8da8f
ivanbiljan <ssbiljan@gmail.com>
2020-09-17 21:38:52 +0200

    Implement GolfBall

---
+++ src/Orion.Core/Packets/World/GolfBall.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent when a player pockets a golf ball.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 9)]
+    public struct GolfBall : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cup's X position.
+        /// </summary>
+        [field: FieldOffset(1)] public ushort X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cup's Y position.
+        /// </summary>
+        [field: FieldOffset(3)] public ushort Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of hits.
+        /// </summary>
+        [field: FieldOffset(5)] public ushort NumberOfHits { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ball's projectile ID.
+        /// </summary>
+        [field: FieldOffset(7)] public ushort ProjectileId { get; set; }
+
+        PacketId IPacket.Id => PacketId.GolfBall;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 9);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 9);
+    }
+}
---
+++ tests/Orion.Core.Tests/World/GolfBallTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Orion.Core.Packets;
+using Orion.Core.Packets.World;
+using Xunit;
+
+namespace Orion.Core.World
+{
+    public sealed class GolfBallTests
+    {
+        private readonly byte[] _bytes = { 12, 0, 128, 1, 2, 0, 3, 0, 4, 0, 5, 0 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new GolfBall();
+
+            packet.PlayerIndex = 1;
+
+            Assert.Equal(1, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new GolfBall();
+
+            packet.X = 1;
+
+            Assert.Equal(1, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new GolfBall();
+
+            packet.Y = 1;
+
+            Assert.Equal(1, packet.Y);
+        }
+
+        [Fact]
+        public void NumberOfHits_Set_Get()
+        {
+            var packet = new GolfBall();
+
+            packet.NumberOfHits = 1;
+
+            Assert.Equal(1, packet.NumberOfHits);
+        }
+
+        [Fact]
+        public void ProjectileId_Set_Get()
+        {
+            var packet = new GolfBall();
+
+            packet.ProjectileId = 1;
+
+            Assert.Equal(1, packet.ProjectileId);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<GolfBall>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.PlayerIndex);
+            Assert.Equal(2, packet.X);
+            Assert.Equal(3, packet.Y);
+            Assert.Equal(4, packet.NumberOfHits);
+            Assert.Equal(5, packet.ProjectileId);
+        }
+    }
+}
