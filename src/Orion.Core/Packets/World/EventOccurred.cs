c04268b47a8a143fec96d5f905a7e90a966859cd
ivanbiljan <ssbiljan@gmail.com>
2020-09-20 20:03:14 +0200

    Implement EventOccurred

---
+++ src/Orion.Core/Packets/World/EventOccurred.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.World
+{
+    /// <summary>
+    /// A packet sent to notify players of an event's progression.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 2)]
+    public struct EventOccurred : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior refrence
+
+        /// <summary>
+        /// Gets or sets the event ID.
+        /// </summary>
+        [field: FieldOffset(0)] public short EventId { get; set; }
+
+        PacketId IPacket.Id => PacketId.EventOccurred;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 2);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 2);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/World/EventOccurredTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.World
+{
+    public sealed class EventOccurredTests
+    {
+        private readonly byte[] _bytes = { 5, 0, 98, 1, 0 };
+
+        [Fact]
+        public void EventId_Set_Get()
+        {
+            var packet = new EventOccurred();
+
+            packet.EventId = 1;
+
+            Assert.Equal(1, packet.EventId);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<EventOccurred>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.EventId);
+        }
+    }
+}
