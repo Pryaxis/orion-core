219641536c99c0fb7cb0b5d13ef3b94ade32dd06
ivanbiljan <ssbiljan@gmail.com>
2020-09-20 19:55:41 +0200

    Implement PlayerPortal

---
+++ src/Orion.Core/Packets/Players/PlayerPortal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Players
+{
+    /// <summary>
+    /// A packet sent to send a player through a portal.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 19)]
+    public struct PlayerPortal : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the player index.
+        /// </summary>
+        [field: FieldOffset(0)] public byte PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the portal color index.
+        /// </summary>
+        [field: FieldOffset(1)] public short PortalColorIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X position.
+        /// </summary>
+        [field: FieldOffset(3)] public float X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y position.
+        /// </summary>
+        [field: FieldOffset(7)] public float Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the horizontal velocity.
+        /// </summary>
+        [field: FieldOffset(11)] public float VelocityX { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vertical velocity.
+        /// </summary>
+        [field: FieldOffset(15)] public float VelocityY { get; set; }
+
+        PacketId IPacket.Id => PacketId.PlayerPortal;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 19);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 19);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Players/PlayerPortalTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Players
+{
+    public sealed class PlayerPortalTests
+    {
+        private readonly byte[] _bytes = { 22, 0, 96, 1, 2, 0, 0, 0, 0, 64, 0, 0, 0, 64, 0, 0, 0, 64, 0, 0, 0, 64 };
+
+        [Fact]
+        public void PlayerIndex_Set_Get()
+        {
+            var packet = new PlayerPortal();
+
+            packet.PlayerIndex = 1;
+
+            Assert.Equal(1, packet.PlayerIndex);
+        }
+
+        [Fact]
+        public void PortalColorIndex_Set_Get()
+        {
+            var packet = new PlayerPortal();
+
+            packet.PortalColorIndex = 1;
+
+            Assert.Equal(1, packet.PortalColorIndex);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new PlayerPortal();
+
+            packet.X = 1;
+
+            Assert.Equal(1, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new PlayerPortal();
+
+            packet.Y = 1;
+
+            Assert.Equal(1, packet.Y);
+        }
+
+        [Fact]
+        public void VelocityX_Set_Get()
+        {
+            var packet = new PlayerPortal();
+
+            packet.VelocityX = 1;
+
+            Assert.Equal(1, packet.VelocityX);
+        }
+
+        [Fact]
+        public void VelocityY_Set_Get()
+        {
+            var packet = new PlayerPortal();
+
+            packet.VelocityY = 1;
+
+            Assert.Equal(1, packet.VelocityY);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<PlayerPortal>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.PlayerIndex);
+            Assert.Equal(2, packet.PortalColorIndex);
+            Assert.Equal(2, packet.X);
+            Assert.Equal(2, packet.Y);
+            Assert.Equal(2, packet.VelocityX);
+            Assert.Equal(2, packet.VelocityY);
+        }
+    }
+}
