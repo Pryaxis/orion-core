31709825f6140eafde6d71b800712fc51419ce24
ivanbiljan <ssbiljan@gmail.com>
2020-09-22 14:10:01 +0200

    Implement NpcMerchant

---
+++ src/Orion.Core/Packets/Npcs/NpcMerchant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent to update a merchant's inventory.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 80)]
+    public struct NpcMerchant : IPacket
+    {
+        [FieldOffset(0)] private short[]? _shopItems;
+
+        /// <summary>
+        /// Gets the shop.
+        /// </summary>
+        public short[] ShopItems => _shopItems ??= new short[40];
+
+        PacketId IPacket.Id => PacketId.NpcMerchant;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) =>
+            span.Read(ref Unsafe.As<short, byte>(ref ShopItems[0]), 80);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) =>
+            span.Write(ref Unsafe.As<short, byte>(ref ShopItems[0]), 80);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcMerchantTests.cs
@@ -0,0 +1,38 @@
+﻿using ReLogic.Content;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    public sealed class NpcMerchantTests
+    {
+        private readonly byte[] _bytes =
+        {
+            83, 0, 72, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10, 0, 11, 0, 12, 0, 13, 0, 14, 0, 15,
+            0, 16, 0, 17, 0, 18, 0, 19, 0, 20, 0, 21, 0, 22, 0, 23, 0, 24, 0, 25, 0, 26, 0, 27, 0, 28, 0, 29, 0, 30,
+            0, 31, 0, 32, 0, 33, 0, 34, 0, 35, 0, 36, 0, 37, 0, 38, 0, 39, 0, 40, 0
+        };
+
+        [Fact]
+        public void ShopItems_Set_Get()
+        {
+            var packet = new NpcMerchant();
+
+            packet.ShopItems[0] = 0;
+            packet.ShopItems[1] = 1;
+            
+            Assert.Equal(0, packet.ShopItems[0]);
+            Assert.Equal(1, packet.ShopItems[1]);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<NpcMerchant>(_bytes, PacketContext.Server);
+
+            for (int i = 0; i < packet.ShopItems.Length; i++)
+            {
+                Assert.Equal(i + 1, packet.ShopItems[i]);
+            }
+        }
+    }
+}
