4420625521acfe5a1b95ddddb468082386aa089d
ivanbiljan <ssbiljan@gmail.com>
2020-09-27 01:14:49 +0200

    Implement NpcPortal

---
+++ src/Orion.Core/Packets/Npcs/NpcPortal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent to teleport NPC's through portals.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct NpcPortal : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the NPC index.
+        /// </summary>
+        [field: FieldOffset(0)] public ushort NpcIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the portal color index.
+        /// </summary>
+        [field: FieldOffset(2)] public short PortalColorIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new position.
+        /// </summary>
+        [field: FieldOffset(4)] public Vector2f NewPosition { get; set; }
+
+        /// <summary>
+        /// Gets or sets the velocity.
+        /// </summary>
+        [field: FieldOffset(12)] public Vector2f Velocity { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcPortal;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 20);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 20);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcPortalTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Orion.Core.Utils;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    public sealed class NpcPortalTests
+    {
+        private readonly byte[] _bytes = { 23, 0, 100, 1, 0, 2, 0, 0, 0, 0, 64, 0, 0, 0, 64, 0, 0, 0, 64, 0, 0, 0, 64 };
+
+        [Fact]
+        public void NpcIndex_Set_Get()
+        {
+            var packet = new NpcPortal();
+
+            packet.NpcIndex = 1;
+
+            Assert.Equal(1, packet.NpcIndex);
+        }
+
+        [Fact]
+        public void PortalColorIndex_Set_Get()
+        {
+            var packet = new NpcPortal();
+
+            packet.PortalColorIndex = 1;
+
+            Assert.Equal(1, packet.PortalColorIndex);
+        }
+
+        [Fact]
+        public void NewPosition_Set_Get()
+        {
+            var packet = new NpcPortal();
+
+            packet.NewPosition = new Vector2f(1.23F, 4.56F);
+
+            Assert.Equal(new Vector2f(1.23F, 4.56F), packet.NewPosition);
+        }
+
+        [Fact]
+        public void Velocity_Set_Get()
+        {
+            var packet = new NpcPortal();
+
+            packet.Velocity = new Vector2f(1.23F, 4.56F);
+
+            Assert.Equal(new Vector2f(1.23F, 4.56F), packet.Velocity);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<NpcPortal>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.NpcIndex);
+            Assert.Equal(2, packet.PortalColorIndex);
+            Assert.Equal(new Vector2f(2F, 2F), packet.NewPosition);
+            Assert.Equal(new Vector2f(2F, 2F), packet.Velocity);
+        }
+    }
+}
