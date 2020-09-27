598bb67d20835876ab71631bdf3629c3ae22c2ce
ivanbiljan <ssbiljan@gmail.com>
2020-09-27 19:40:04 +0200

    Implement ProjectileCannon

---
+++ src/Orion.Core/Packets/Projectiles/ProjectileCannon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Projectiles
+{
+    /// <summary>
+    /// A packet sent to shoot projectiles from cannons.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 15)]
+    public struct ProjectileCannon : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the damage.
+        /// </summary>
+        [field: FieldOffset(0)] public short Damage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the knockback.
+        /// </summary>
+        [field: FieldOffset(2)] public float Knockback { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X coordinate.
+        /// </summary>
+        [field: FieldOffset(6)] public short X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y coordinate.
+        /// </summary>
+        [field: FieldOffset(8)] public short Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the shooting angle.
+        /// </summary>
+        [field: FieldOffset(10)] public short Angle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ammo.
+        /// </summary>
+        [field: FieldOffset(12)] public short Ammo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the owner index.
+        /// </summary>
+        [field: FieldOffset(14)] public byte OwnerIndex { get; set; }
+
+        PacketId IPacket.Id => PacketId.ProjectileCannon;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 15);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 15);
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Projectiles/ProjectileCannonTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReLogic.Content;
+using Xunit;
+
+namespace Orion.Core.Packets.Projectiles
+{
+    public sealed class ProjectileCannonTests
+    {
+        private readonly byte[] _bytes = { 18, 0, 108, 1, 0, 0, 0, 0, 64, 3, 0, 4, 0, 5, 0, 6, 0, 7 };
+
+        [Fact]
+        public void Damage_Set_Get()
+        {
+            var packet = new ProjectileCannon();
+
+            packet.Damage = 1;
+
+            Assert.Equal(1, packet.Damage);
+        }
+
+        [Fact]
+        public void Knockback_Set_Get()
+        {
+            var packet = new ProjectileCannon();
+
+            packet.Knockback = 1;
+
+            Assert.Equal(1, packet.Knockback);
+        }
+
+        [Fact]
+        public void X_Set_Get()
+        {
+            var packet = new ProjectileCannon();
+
+            packet.X = 1;
+
+            Assert.Equal(1, packet.X);
+        }
+
+        [Fact]
+        public void Y_Set_Get()
+        {
+            var packet = new ProjectileCannon();
+
+            packet.Y = 1;
+
+            Assert.Equal(1, packet.Y);
+        }
+
+        [Fact]
+        public void Angle_Set_Get()
+        {
+            var packet = new ProjectileCannon();
+
+            packet.Angle = 1;
+
+            Assert.Equal(1, packet.Angle);
+        }
+
+        [Fact]
+        public void Ammo_Set_Get()
+        {
+            var packet = new ProjectileCannon();
+
+            packet.Ammo = 1;
+
+            Assert.Equal(1, packet.Ammo);
+        }
+
+        [Fact]
+        public void OwnerIndex_Set_Get()
+        {
+            var packet = new ProjectileCannon();
+
+            packet.OwnerIndex = 1;
+
+            Assert.Equal(1, packet.OwnerIndex);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<ProjectileCannon>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.Damage);
+            Assert.Equal(2F, packet.Knockback);
+            Assert.Equal(3, packet.X);
+            Assert.Equal(4, packet.Y);
+            Assert.Equal(5, packet.Angle);
+            Assert.Equal(6, packet.Ammo);
+            Assert.Equal(7, packet.OwnerIndex);
+        }
+    }
+}
