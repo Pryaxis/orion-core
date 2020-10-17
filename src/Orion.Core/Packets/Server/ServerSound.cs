ced34bc8236bc3416aa9236f8b52d7933c12a538
ivanbiljan <ssbiljan@gmail.com>
2020-10-17 17:09:34 +0200

    Implement ServerSound

---
+++ src/Orion.Core/Packets/Server/ServerSound.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Orion.Core.Packets.DataStructures;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Server
+{
+    /// <summary>
+    /// A packet sent to play a sound.
+    /// </summary>
+    public struct ServerSound : IPacket
+    {
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
+        public Vector2f Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sound index.
+        /// </summary>
+        public ushort SoundIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the style.
+        /// </summary>
+        public int? Style { get; set; }
+
+        /// <summary>
+        /// Gets or sets the volume.
+        /// </summary>
+        public float? Volume { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pitch offset.
+        /// </summary>
+        public float? PitchOffset { get; set; }
+
+        PacketId IPacket.Id => PacketId.ServerSound;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context)
+        {
+            var length = 11;
+            Position = span.Read<Vector2f>();
+            SoundIndex = span.Read<ushort>();
+            var flags = span.Read<Flags8>();
+            if (flags[0])
+            {
+                Style = span.Read<int>();
+                length += 4;
+            }
+            if (flags[1])
+            {
+                Volume = span.Read<float>();
+                length += 4;
+            }
+            if (flags[2])
+            {
+                PitchOffset = span.Read<float>();
+                length += 4;
+            }
+                
+            return length;
+        }
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context)
+        {
+            var length = span.Write(Position);
+            var flags = default(Flags8);
+            length += span[length..].Write(SoundIndex);
+
+            var flagsOffset = length++;
+            if (Style.HasValue)
+            {
+                flags[0] = true;
+                length += span[length..].Write(Style.Value);
+            }
+            if (Volume.HasValue)
+            {
+                flags[1] = true;
+                length += span[length..].Write(Volume.Value);
+            }
+            if (PitchOffset.HasValue)
+            {
+                flags[2] = true;
+                length += span[length..].Write(PitchOffset.Value);
+            }
+
+            span[flagsOffset] = Unsafe.As<Flags8, byte>(ref flags);
+            return length;
+        }
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Server/ServerSoundTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Orion.Core.Utils;
+using Xunit;
+
+namespace Orion.Core.Packets.Server
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public sealed class ServerSoundTests
+    {
+        private readonly byte[] _bytes = { 26, 0, 132, 164, 112, 157, 63, 133, 235, 145, 64, 2, 0, 7, 3, 0, 0, 0, 0, 0, 128, 64, 0, 0, 160, 64 };
+
+        [Fact]
+        public void Position_Set_Get()
+        {
+            var packet = new ServerSound();
+
+            packet.Position = new Vector2f(1.23F, 4.56F);
+
+            Assert.Equal(new Vector2f(1.23F, 4.56F), packet.Position);
+        }
+
+        [Fact]
+        public void SoundIndex_Set_Get()
+        {
+            var packet = new ServerSound();
+
+            packet.SoundIndex = 1;
+
+            Assert.Equal(1, packet.SoundIndex);
+        }
+
+        [Fact]
+        public void Style_Set_Get()
+        {
+            var packet = new ServerSound();
+
+            packet.Style = 1;
+
+            Assert.Equal(1, packet.Style);
+        }
+
+        [Fact]
+        public void Volume_Set_Get()
+        {
+            var packet = new ServerSound();
+
+            packet.Volume = 1F;
+
+            Assert.Equal(1F, packet.Volume);
+        }
+
+        [Fact]
+        public void PitchOffset_Set_Get()
+        {
+            var packet = new ServerSound();
+
+            packet.PitchOffset = 1F;
+
+            Assert.Equal(1F, packet.PitchOffset);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<ServerSound>(_bytes, PacketContext.Server);
+
+            Assert.Equal(new Vector2f(1.23F, 4.56F), packet.Position);
+            Assert.Equal(2, packet.SoundIndex);
+            Assert.Equal(3, packet.Style!.Value);
+            Assert.Equal(4, packet.Volume!.Value);
+            Assert.Equal(5, packet.PitchOffset!.Value);
+        }
+    }
+}
