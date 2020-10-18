df1be351ea0123981549d782d964dba27e44e6db
ivanbiljan <ssbiljan@gmail.com>
2020-10-18 07:00:13 +0200

    Implement NpcEmote

---
+++ src/Orion.Core/Packets/Npcs/NpcEmote.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) 2020 Pryaxis & Orion Contributors
+// 
+// This file is part of Orion.
+// 
+// Orion is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Orion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orion.Core.Utils;
+
+namespace Orion.Core.Packets.Npcs
+{
+    /// <summary>
+    /// A packet sent to display an NPC emote.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 10)]
+    public struct NpcEmote : IPacket
+    {
+        [FieldOffset(0)] private byte _bytes; // Used to obtain an interior reference
+
+        /// <summary>
+        /// Gets or sets the emote bubble ID.
+        /// </summary>
+        [field: FieldOffset(0)] public int EmoteBubbleId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the world UI anchor type.
+        /// </summary>
+        [field: FieldOffset(4)] public WorldUiAnchorType UiAnchorType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the anchor entity's index.
+        /// </summary>
+        [field: FieldOffset(5)] public ushort AnchorEntityIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the emote's lifetime.
+        /// </summary>
+        [field: FieldOffset(7)] public ushort LifeTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the emote ID.
+        /// </summary>
+        [field: FieldOffset(9)] public byte EmoteId { get; set; }
+
+        PacketId IPacket.Id => PacketId.NpcEmote;
+
+        int IPacket.ReadBody(Span<byte> span, PacketContext context) => span.Read(ref _bytes, 10);
+
+        int IPacket.WriteBody(Span<byte> span, PacketContext context) => span.Write(ref _bytes, 10);
+    }
+
+    /// <summary>
+    /// Specifies a user interface anchor.
+    /// </summary>
+    public enum WorldUiAnchorType : byte
+    {
+        /// <summary>
+        /// Indicates an NPC.
+        /// </summary>
+        Npc,
+
+        /// <summary>
+        /// Indicates a player.
+        /// </summary>
+        Player,
+
+        /// <summary>
+        /// Indicates a projectile.
+        /// </summary>
+        Projectile
+    }
+}
---
+++ tests/Orion.Core.Tests/Packets/Npcs/NpcEmoteTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xunit;
+
+namespace Orion.Core.Packets.Npcs
+{
+    [SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Testing")]
+    public sealed class NpcEmoteTests
+    {
+        private readonly byte[] _bytes = { 13, 0, 91, 1, 0, 0, 0, 0, 2, 0, 3, 0, 4 };
+
+        [Fact]
+        public void EmoteBubbleId_Set_Get()
+        {
+            var packet = new NpcEmote();
+
+            packet.EmoteBubbleId = 1;
+
+            Assert.Equal(1, packet.EmoteBubbleId);
+        }
+
+        [Theory]
+        [InlineData(WorldUiAnchorType.Npc)]
+        [InlineData(WorldUiAnchorType.Player)]
+        [InlineData(WorldUiAnchorType.Projectile)]
+        public void UiAnchorType_Set_Get(WorldUiAnchorType value)
+        {
+            var packet = new NpcEmote();
+
+            packet.UiAnchorType = value;
+
+            Assert.Equal(value, packet.UiAnchorType);
+        }
+
+        [Fact]
+        public void AnchorEntityIndex_Set_Get()
+        {
+            var packet = new NpcEmote();
+
+            packet.AnchorEntityIndex = 1;
+
+            Assert.Equal(1, packet.AnchorEntityIndex);
+        }
+
+        [Fact]
+        public void LifeTime_Set_Get()
+        {
+            var packet = new NpcEmote();
+
+            packet.LifeTime = 1;
+
+            Assert.Equal(1, packet.LifeTime);
+        }
+
+        [Fact]
+        public void EmoteId_Set_Get()
+        {
+            var packet = new NpcEmote();
+
+            packet.EmoteId = 1;
+
+            Assert.Equal(1, packet.EmoteId);
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var packet = TestUtils.ReadPacket<NpcEmote>(_bytes, PacketContext.Server);
+
+            Assert.Equal(1, packet.EmoteBubbleId);
+            Assert.Equal(WorldUiAnchorType.Npc, packet.UiAnchorType);
+            Assert.Equal(2, packet.AnchorEntityIndex);
+            Assert.Equal(3, packet.LifeTime);
+            Assert.Equal(4, packet.EmoteId);
+        }
+    }
+}
